f44822322e444f22c7b7846d2a5906dd1e9628fe
valerii <vsbrudanin@gmail.com>
2023-11-09 17:45:50 +0100

    Surface levels added

--- unittests/test_image_processing.py
+++ unittests/test_image_processing.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import unittest
 import time
+import skimage
 from matplotlib.widgets import Slider
 import tqdm
 
@@ -29,15 +30,15 @@ s_polarized_waves = {
     (0, 2, 0)  : Sources.IntensityPlaneWave((-b ** 2) / norm, 0, np.array((0, 2 * k1, 0))),
     (0, -2, 0) : Sources.IntensityPlaneWave((-b ** 2) / norm, 0, np.array((0, -2 * k1, 0))),
 
-    (1, 0, 1)  : Sources.IntensityPlaneWave((-1j * b) / norm, 0, np.array((k1, 0, k2))),
-    (-1, 0, 1) : Sources.IntensityPlaneWave((1j * b) / norm, 0, np.array((-k1, 0, k2))),
-    (0, 1, 1)  : Sources.IntensityPlaneWave((-1 * b) / norm, 0, np.array((0, k1, k2))),
-    (0, -1, 1) : Sources.IntensityPlaneWave((1 * b) / norm, 0, np.array((0, -k1, k2))),
-
-    (1, 0, -1) : Sources.IntensityPlaneWave((-1j * b) / norm, 0, np.array((k1, 0, -k2))),
-    (-1, 0, -1): Sources.IntensityPlaneWave((1j * b) / norm, 0, np.array((-k1, 0, -k2))),
-    (0, 1, -1) : Sources.IntensityPlaneWave((1 * b) / norm, 0, np.array((0, k1, -k2))),
-    (0, -1, -1): Sources.IntensityPlaneWave((-1 * b) / norm, 0, np.array((0, -k1, -k2)))
+    # (1, 0, 1)  : Sources.IntensityPlaneWave((-1j * b) / norm, 0, np.array((k1, 0, k2))),
+    # (-1, 0, 1) : Sources.IntensityPlaneWave((1j * b) / norm, 0, np.array((-k1, 0, k2))),
+    # (0, 1, 1)  : Sources.IntensityPlaneWave((-1 * b) / norm, 0, np.array((0, k1, k2))),
+    # (0, -1, 1) : Sources.IntensityPlaneWave((1 * b) / norm, 0, np.array((0, -k1, k2))),
+    #
+    # (1, 0, -1) : Sources.IntensityPlaneWave((-1j * b) / norm, 0, np.array((k1, 0, -k2))),
+    # (-1, 0, -1): Sources.IntensityPlaneWave((1j * b) / norm, 0, np.array((-k1, 0, -k2))),
+    # (0, 1, -1) : Sources.IntensityPlaneWave((1 * b) / norm, 0, np.array((0, k1, -k2))),
+    # (0, -1, -1): Sources.IntensityPlaneWave((-1 * b) / norm, 0, np.array((0, -k1, -k2)))
 }
 
 Mt_circular = 32
@@ -227,24 +228,28 @@ class TestOpticalSystems(unittest.TestCase):
         waves = s_polarized_waves
 
         illumination_polarized = ImageProcessing.Illumination(waves)
-        optical_system = ImageProcessing.Lens(interpolation_method="Fourier")
-        begin_shifted = time.time()
-        optical_system.compute_psf_and_otf((np.array((2 * max_r, 2 * max_r, 2 * max_z)), N))
+        optical_system_fourier = ImageProcessing.Lens(interpolation_method="Fourier")
+        optical_system_fourier.compute_psf_and_otf((np.array((2 * max_r, 2 * max_r, 2 * max_z)), N),
+                                                   apodization_filter=None)
+        optical_system_linear = ImageProcessing.Lens()
+        optical_system_linear.compute_psf_and_otf((np.array((2 * max_r, 2 * max_r, 2 * max_z)), N),
+                                                  apodization_filter=None)
         wavevectors = illumination_polarized.get_wavevectors()
-        optical_system.prepare_Fourier_interpolation(wavevectors)
 
-        noise_estimator = ImageProcessing.NoiseEstimator(illumination_polarized, optical_system)
+        noise_estimator = ImageProcessing.NoiseEstimator(illumination_polarized, optical_system_fourier)
 
+        begin_shifted = time.time()
+        optical_system_fourier.prepare_Fourier_interpolation(wavevectors)
         q_axes = 2 * np.pi * np.array((fx, fy, fz))
-        SSNR = np.abs(noise_estimator.SSNR(q_axes))
+        SSNR_F = np.abs(noise_estimator.SSNR(q_axes))
         end_shifted = time.time()
-        print(end_shifted - begin_shifted)
+        print("fourier time is ", end_shifted - begin_shifted)
 
-        # begin_interpolation = time.time()
-        # SSNR_I = np.zeros((N, N, N))
-        # SSNR_I = np.abs(noise_estimator.SSNR(q_axes, method="Scipy"))
-        # end_interpolation = time.time()
-        # print(end_interpolation - begin_interpolation)
+        noise_estimator.optical_system = optical_system_linear
+        begin_interpolation = time.time()
+        SSNR_L = np.abs(noise_estimator.SSNR(q_axes))
+        end_interpolation = time.time()
+        print("linear time is ", end_interpolation - begin_interpolation)
 
         Fx, Fy = np.meshgrid(fx, fy)
         fig = plt.figure()
@@ -256,41 +261,40 @@ class TestOpticalSystems(unittest.TestCase):
                             hspace=0.4)
         ax1 = fig.add_subplot(121)
         ax2 = fig.add_subplot(122)
-        mp1 = ax1.imshow(np.log10(SSNR[:, :, int(N / 2)]))
+        mp1 = ax1.imshow(np.log10(SSNR_F[:, :, int(N / 2)]))
         cb1 = plt.colorbar(mp1, fraction=0.046, pad=0.04)
-        # mp2 = ax2.imshow(np.log10(SSNR_I[:, :, int(N / 2)]))
-        # cb2 = plt.colorbar(mp2, fraction=0.046, pad=0.04)
+        mp2 = ax2.imshow(np.log10(SSNR_L[:, :, int(N / 2)]))
+        cb2 = plt.colorbar(mp2, fraction=0.046, pad=0.04)
 
         def update1(val):
-            ax1.set_title("SSNR, fz = {:.2f}, ".format(fz[int(val)]) + "$\\lambda^{-1}$")
+            ax1.set_title("SSNR_F, fz = {:.2f}, ".format(fz[int(val)]) + "$\\lambda^{-1}$")
             ax1.set_xlabel("fx, $\lambda^{-1}$")
             ax1.set_ylabel("fy, $\lambda^{-1}$")
-            Z = (np.log10(SSNR[:, :, int(val)]))
+            Z = (np.log10(SSNR_F[:, :, int(val)]))
             mp1.set_data(Z)
             mp1.set_clim(vmin=Z.min(), vmax=Z.max())
             plt.draw()
-            print(np.amax(SSNR[:, :, int(val)]))
+            print(np.amax(SSNR_F[:, :, int(val)]))
 
         slider_loc = plt.axes((0.2, 0.1, 0.3, 0.03))  # slider location and size
         slider_ssnr = Slider(slider_loc, 'fz', 0, N - 1)  # slider properties
         slider_ssnr.on_changed(update1)
 
-        # def update2(val):
-        #     ax2.set_title("SSNR_I, fz = {:.2f}, ".format(fz[int(val)]) + "$\\lambda^{-1}$")
-        #     ax2.set_xlabel("fx, $\lambda^{-1}$")
-        #     ax2.set_ylabel("fy, $\lambda^{-1}$")
-        #     print(int(val))
-        #     Z = (np.log10(SSNR_I[:, :, int(val)]))
-        #     mp2.set_data(Z)
-        #     min = Z.min()
-        #     max = Z.max()
-        #     mp2.set_clim(vmin=min, vmax=max)
-        #     # plt.draw()
-        #     print(np.amax(np.abs(SSNR[:, :, int(val)])))
-        #
-        # slider_loc = plt.axes((0.6, 0.1, 0.3, 0.03))  # slider location and size
-        # slider_ssnri = Slider(slider_loc, 'fz', 0, N - 1)  # slider properties
-        # slider_ssnri.on_changed(update2)
+        def update2(val):
+            ax2.set_title("SSNR_L, fz = {:.2f}, ".format(fz[int(val)]) + "$\\lambda^{-1}$")
+            ax2.set_xlabel("fx, $\lambda^{-1}$")
+            ax2.set_ylabel("fy, $\lambda^{-1}$")
+            print(int(val))
+            Z = (np.log10(SSNR_L[:, :, int(val)]))
+            mp2.set_data(Z)
+            min = Z.min()
+            max = Z.max()
+            mp2.set_clim(vmin=min, vmax=max)
+            # plt.draw()
+
+        slider_loc = plt.axes((0.6, 0.1, 0.3, 0.03))  # slider location and size
+        slider_ssnri = Slider(slider_loc, 'fz', 0, N - 1)  # slider properties
+        slider_ssnri.on_changed(update2)
 
         plt.show()
 
@@ -309,19 +313,15 @@ class TestOpticalSystems(unittest.TestCase):
         fy = np.linspace(-1 / (2 * dy), 1 / (2 * dy) - 1 / (2 * max_r), N)
         fz = np.linspace(-1 / (2 * dz), 1 / (2 * dz) - 1 / (2 * max_r), N)
 
-        waves = three_waves_illumination
+        waves = s_polarized_waves
 
-        illumination_polarized = ImageProcessing.Illumination(waves, M_r=3)
+        illumination_polarized = ImageProcessing.Illumination(waves, M_r=1)
         illumination_polarized.M_t = 32
         optical_system = ImageProcessing.Lens(alpha=np.pi/4)
 
-        optical_system.compute_PSF_and_OTF(np.array((2 * max_r, 2 * max_r, 2 * max_z)), N)
-        optical_system.compute_shifted_otf(illumination_polarized.get_wavevectors())
-        optical_system.compute_wvdiff_otfs(illumination_polarized.get_wavevectors())
+        optical_system.compute_psf_and_otf(((2 * max_r, 2 * max_r, 2 * max_z), N))
 
         noise_estimator = ImageProcessing.NoiseEstimator(illumination_polarized, optical_system)
-        noise_estimator.compute_parameters_for_Vj()
-        noise_estimator.compute_wfdiff_otfs_for_Vj()
 
         q_axes = 2 * np.pi * np.array((fx, fy, fz))
         SSNR = np.abs(noise_estimator.SSNR(q_axes,))
@@ -382,6 +382,67 @@ class TestOpticalSystems(unittest.TestCase):
 
         plt.show()
 
+    def test_isosurface_visualisation(self):
+        max_r = 6
+        max_z = 6
+        N = 80
+        dx = 2 * max_r / N
+        dy = 2 * max_r / N
+        dz = 2 * max_z / N
+        x = np.arange(-max_r, max_r, dx)
+        y = np.copy(x)
+        z = np.arange(-max_z, max_z, dz)
+
+        fx = np.linspace(-1 / (2 * dx), 1 / (2 * dx) - 1 / (2 * max_r), N)
+        fy = np.linspace(-1 / (2 * dy), 1 / (2 * dy) - 1 / (2 * max_r), N)
+        fz = np.linspace(-1 / (2 * dz), 1 / (2 * dz) - 1 / (2 * max_r), N)
+
+        waves = s_polarized_waves
+
+        illumination_polarized = ImageProcessing.Illumination(waves, M_r=1)
+        illumination_polarized.M_t = 32
+        optical_system = ImageProcessing.Lens(alpha=np.pi/4)
+
+        optical_system.compute_psf_and_otf(((2 * max_r, 2 * max_r, 2 * max_z), N))
+
+        noise_estimator = ImageProcessing.NoiseEstimator(illumination_polarized, optical_system)
+
+        q_axes = 2 * np.pi * np.array((fx, fy, fz))
+        SSNR = np.abs(noise_estimator.SSNR(q_axes,))
+        SSNR_scaled = np.log10(1 + 10**8 * np.abs(SSNR))
+        constant_value = SSNR_scaled[N//2, N//2, N//2]
+        print(np.amax(SSNR_scaled))
+        verts, faces, _, _ = skimage.measure.marching_cubes(SSNR_scaled, level = np.amax(SSNR_scaled)/2 )
+        Fx, Fy, Fz = np.meshgrid(fx, fy, fz)
+        fig = plt.figure()
+        plt.subplots_adjust(left=0.1,
+                            bottom=0.1,
+                            right=0.9,
+                            top=0.9,
+                            wspace=0.4,
+                            hspace=0.4)
+        ax1 = fig.add_subplot(111, projection = '3d')
+        ax1.set_title("$log(1 + 10^8 SSNR)$" +"= {}".format(3))
+        ax1.set_xlabel("fx, $\lambda^{-1}$")
+        ax1.set_ylabel("fy, $\lambda^{-1}$")
+        ax1.set_zlabel("fz, $\lambda^{-1}$")
+        indices = np.where(np.abs(SSNR - 10**-5) < 10**-7)
+        ax1.plot_trisurf(verts[:, 0], verts[:, 1], faces, verts[:, 2],  alpha=0.7)
+
+        def update1(val):
+            ax1.clear()
+            ax1.set_title("$log(1 + 10^8 SSNR)$" + "= {:.2f}".format(val/10))
+            ax1.set_xlabel("fx, $\lambda^{-1}$")
+            ax1.set_ylabel("fy, $\lambda^{-1}$")
+            ax1.set_zlabel("fz, $\lambda^{-1}$")
+            verts, faces, _, _ = skimage.measure.marching_cubes(SSNR_scaled, level=val/10)
+            ax1.plot_trisurf(verts[:, 0], verts[:, 1], faces, verts[:, 2], alpha=0.7)
+            plt.draw()
+
+        slider_loc = plt.axes((0.2, 0.1, 0.3, 0.03))  # slider location and size
+        slider_ssnr = Slider(slider_loc, 'fz', 0, 50)  # slider properties
+        slider_ssnr.on_changed(update1)
+        plt.show()
     def test_SSNR_projections(self):
         max_r = 6
         max_z = 6
@@ -477,7 +538,7 @@ class TestOpticalSystems(unittest.TestCase):
         fz = np.linspace(-1 / (2 * dz), 1 / (2 * dz) - 1 / (2 * max_z), N)
         q_axes = 2 * np.pi * np.array((fx, fy, fz))
 
-        optical_system = ImageProcessing.Lens(interpolation_method="Fourier")
+        optical_system = ImageProcessing.Lens(interpolation_method="linear")
         optical_system.compute_psf_and_otf((psf_size, N),
                                            apodization_filter=None)
 
