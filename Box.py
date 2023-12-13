Fixed: Numerical peaks search is working

Box.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
-import wrappers
 
+import Sources
+import wrappers
+import stattools
 
 class FieldHolder:
     def __init__(self, source, grid, identifier):
@@ -22,27 +24,47 @@ class Box:
     def __init__(self, sources, box_size, point_number, additional_info=None):
         self.info = additional_info
         self.point_number = point_number
-        self.box_size = box_size
+        if type(box_size) == float or type(box_size) == int:
+            box_size = (box_size, box_size, box_size)
+        self.box_size = np.array(box_size)
+        self.box_volume = self.box_size[0] * self.box_size[1] * self.box_size[2]
         self.fields = []
+        self.numerically_approximated_intensity_fields = []
         self.source_identifier = 0
+        self.axes, self.frequency_axes = self.compute_axes()
         self.grid = np.zeros((self.point_number, self.point_number, self.point_number, 3))
         self.compute_grid()
         self.electric_field = np.zeros((self.point_number, self.point_number, self.point_number, 3),
                                        dtype=np.complex128)
         self.intensity = np.zeros((self.point_number, self.point_number, self.point_number))
+        self.numerically_approximated_intensity = np.zeros((self.point_number, self.point_number, self.point_number))
         self.intensity_fourier_space = np.zeros((self.point_number, self.point_number, self.point_number))
+        self.numerically_approximated_intensity_fourier_space = np.zeros((self.point_number, self.point_number, self.point_number))
         self.analytic_frequencies = []
 
         for source in sources:
-            self.fields.append(FieldHolder(source, self.grid, self.source_identifier))
-            self.source_identifier += 1
+            self.add_source(source)
 
+    def compute_axes(self):
+        N = self.point_number
+        dx = self.box_size[0] / N
+        dy = self.box_size[1] / N
+        dz = self.box_size[2] / N
+
+        x = np.arange(-self.box_size[0]/2, self.box_size[0]/2, dx)
+        y = np.arange(-self.box_size[1]/2, self.box_size[1]/2, dy)
+        z = np.arange(-self.box_size[2]/2, self.box_size[2]/2, dz)
+
+        fx = np.linspace(-1 / (2 * dx), 1 / (2 * dx) - 1 / self.box_size[0], N)
+        fy = np.linspace(-1 / (2 * dy), 1 / (2 * dy) - 1 / self.box_size[1], N)
+        fz = np.linspace(-1 / (2 * dz), 1 / (2 * dz) - 1 / self.box_size[2], N)
+        return (x, y, z), (fx, fy, fz)
     def compute_grid(self):
         indices = np.array(np.meshgrid(np.arange(self.point_number), np.arange(self.point_number),
                                        np.arange(self.point_number))).T.reshape(-1, 3)
         indices = indices[np.lexsort((indices[:, 2], indices[:, 1], indices[:, 0]))].reshape(
             self.point_number, self.point_number, self.point_number, 3)
-        self.grid = self.box_size * (indices / self.point_number - 1 / 2)
+        self.grid = self.box_size[None, None, None, :] * (indices / self.point_number - 1 / 2)
 
     def compute_electric_field(self):
         self.electric_field = np.zeros(self.electric_field.shape, dtype=np.complex128)
@@ -60,13 +82,40 @@ class Box:
                 self.intensity = self.intensity + field.field
         self.intensity = self.intensity.real
 
+    def compute_intensity_and_spacial_waves_numerically(self):
+        self.compute_electric_field()
+        self.compute_intensity_from_electric_field()
+        self.compute_intensity_fourier_space()
+        fourier_peaks, amplitudes = stattools.estimate_localized_peaks(self.intensity_fourier_space, self.frequency_axes)
+        numeric_spacial_waves = []
+        for fourier_peak, amplitude in zip(fourier_peaks, amplitudes):
+            numeric_spacial_waves.append(Sources.IntensityPlaneWave(amplitude, 0, 2 * np.pi * np.array(fourier_peak)))
+        for wave in numeric_spacial_waves:
+            self.numerically_approximated_intensity_fields.append(FieldHolder(wave, self.grid, self.source_identifier))
+            self.source_identifier += 1
+        self.numerically_approximated_intensity = np.zeros(self.intensity.shape, dtype = np.complex128)
+        for field in self.numerically_approximated_intensity_fields:
+            self.numerically_approximated_intensity += field.field
+        self.numerically_approximated_intensity = self.numerically_approximated_intensity.real
+        self.numerically_approximated_intensity_fourier_space = (
+                wrappers.wrapped_ifftn(self.numerically_approximated_intensity) * self.box_volume)
     def compute_intensity_fourier_space(self):
-        self.intensity_fourier_space = wrappers.wrapped_fftn(self.intensity) * (self.box_size / self.point_number) ** 3
-
+        self.intensity_fourier_space = (wrappers.wrapped_fftn(self.intensity) * (self.box_volume / self.point_number ** 3))
     def add_source(self, source):
         self.fields.append(FieldHolder(source, self.grid, self.source_identifier))
         self.source_identifier += 1
 
+    def remove_source(self, source_identifier):
+        for field in self.fields:
+            if field.identifier == source_identifier:
+                self.fields.remove(field)
+                return
+
+    def plot_approximate_intensity_slices(self, ax = None, slider=None):
+        self.plot_slices(self.numerically_approximated_intensity, ax, slider)
+
+    def plot_approximate_intensity_fourier_space_slices(self, ax=None, slider=None):
+        self.plot_slices(self.numerically_approximated_intensity_fourier_space, ax, slider)
     def plot_intensity_slices(self, ax=None, slider=None):
         self.plot_slices(self.intensity, ax, slider)
 
@@ -77,13 +126,13 @@ class Box:
         k_init = self.point_number / 2
         if ax is None:
             fig, ax = plt.subplots()
-        values = (np.arange(self.point_number) / self.point_number - 1 / 2) * self.box_size
-        X, Y = np.meshgrid(values, values)
-        Z = array3d[:, :, int(k_init)].T
+        x, y, z = (np.arange(self.point_number) / self.point_number - 1 / 2) * self.box_size[:, None]
+        X, Y = np.meshgrid(x, y)
+        slice = array3d[:, :, int(k_init)].T
         minValue = np.amin(self.intensity)
         maxValue = min(np.amax(self.intensity), 100.0)
         levels = np.linspace(minValue, maxValue + 1, 30)
-        cf = ax.contourf(X, Y, Z[:, :], levels)
+        cf = ax.contourf(X, Y, slice[:, :], levels)
         plt.colorbar(cf)
         contour_axis = ax
 

GUI.py
@@ -31,8 +31,6 @@ class MainWindow(QMainWindow):
         self.colorbar = None
 
     def init_ui(self):
-        self.setFocusPolicy(Qt.StrongFocus)
-        self.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
         width = 1200
         height = 800
         self.setMinimumSize(width, height)
@@ -90,27 +88,42 @@ class MainWindow(QMainWindow):
         add_spacial_frequency_button = QPushButton("Add a Spatial Frequency")
         add_spacial_frequency_button.clicked.connect(self.add_intensity_plane_wave)
 
+        find_fourier_peaks_numerically = QPushButton("Find Fourier peaks numerically")
+        find_fourier_peaks_numerically.clicked.connect(self.compute_numerically_approximated_intensities)
+        
         self.source_buttons_layout.addStretch()
         self.source_buttons_layout.addWidget(add_plane_wave_button)
         self.source_buttons_layout.addWidget(add_spacial_frequency_button)
+        self.source_buttons_layout.addWidget(find_fourier_peaks_numerically)
         self.source_buttons_layout.addStretch()
 
         # Initialization layout
-        self.initialization_layout = QHBoxLayout()
-        setup_sources_button = QPushButton("Set up from sources")
-        setup_sources_button.clicked.connect(self.compute_and_plot_from_electric_field)
-        setup_frequencies_button = QPushButton("Set up from spatial frequencies")
-        setup_frequencies_button.clicked.connect(self.compute_and_plot_from_intensity_sources)
-        setup_fourier_space_button = QPushButton("Fourier space")
-        setup_fourier_space_button.clicked.connect(self.compute_and_plot_fourier_space)
-        self.initialization_layout.addWidget(setup_sources_button, 1)
-        self.initialization_layout.addWidget(setup_frequencies_button, 1)
-        self.initialization_layout.addWidget(setup_fourier_space_button, 1)
+        self.plot_layout = QHBoxLayout()
 
+        plot_from_electric_fields_button = QPushButton("Intensity from electric fields")
+        plot_from_electric_fields_button.clicked.connect(self.compute_and_plot_from_electric_field)
+
+        plot_from_frequencies_button = QPushButton("Intensity from spatial frequencies")
+        plot_from_frequencies_button.clicked.connect(self.compute_and_plot_from_intensity_sources)
+
+        plot_fourier_space_button = QPushButton("Fourier space")
+        plot_fourier_space_button.clicked.connect(self.compute_and_plot_fourier_space)
+
+        plot_approximate_intensity_button = QPushButton("Approximate intensity")
+        plot_approximate_intensity_button.clicked.connect(self.plot_numerically_approximated_intensity)
+
+        plot_approximate_intensity_fourier_space_button = QPushButton("Approximate Fourier space")
+        plot_approximate_intensity_fourier_space_button.clicked.connect(self.plot_numerically_approximated_intensity_fourier_space)
+
+        self.plot_layout.addWidget(plot_from_electric_fields_button, 1)
+        self.plot_layout.addWidget(plot_from_frequencies_button, 1)
+        self.plot_layout.addWidget(plot_fourier_space_button, 1)
+        self.plot_layout.addWidget(plot_approximate_intensity_button, 1)
+        self.plot_layout.addWidget(plot_approximate_intensity_fourier_space_button, 1)
         # Add sections to the main layout
         self.main_layout.addLayout(self.options_layout, 1)
         self.main_layout.addLayout(self.config_layout, 8)
-        self.main_layout.addLayout(self.initialization_layout, 1)
+        self.main_layout.addLayout(self.plot_layout, 1)
 
         self.setWindowTitle("Interference patterns GUI")
 
@@ -165,6 +178,8 @@ class MainWindow(QMainWindow):
         if type(source) == Sources.PlaneWave:
             self.add_plane_wave(source)
 
+    def remove_source(self, initializer):
+        self.box.remove_source(initializer)
     def add_point_source(self):
         source = GUIWidgets.PointSourceWidget()
         self.sources_layout.addWidget(source)
@@ -180,11 +195,13 @@ class MainWindow(QMainWindow):
         source = GUIWidgets.PlaneWaveWidget(ipw)
         self.sources_layout.addWidget(source)
         source.isSet.connect(lambda initialized: self.add_to_box(initialized, source.plane_wave))
+        source.isDeleted.connect(lambda identifier: self.remove_source(identifier))
 
     def add_intensity_plane_wave(self, ipw=None):
         source = GUIWidgets.IntensityPlaneWaveWidget(ipw)
         self.sources_layout.addWidget(source)
         source.isSet.connect(lambda initialized: self.add_to_box(initialized, source.intensity_plane_wave))
+        source.isDeleted.connect(lambda identifier: self.remove_source(identifier))
 
     def plotting_mode(self, Z, mode="linear"):
         if mode == "linear":
@@ -194,7 +211,54 @@ class MainWindow(QMainWindow):
         elif mode == "mixed":
             return np.log10(1 + Z)
 
-    def plot_fourier_space_slices(self, mode="linear"):
+    def plot_intensity_slices(self, intensity = None):
+        self.canvas.figure.clear()
+        ax = self.canvas.figure.add_subplot(111)
+        ax.set_aspect('equal')
+
+        intensity = self.box.intensity.real if intensity is None else intensity
+
+        if not self.slider:
+            self.slider = QSlider(Qt.Horizontal)  # Horizontal slider
+            self.canvas_layout.addWidget(self.slider)
+            self.slider.setMinimum(0)
+            self.slider.setMaximum(self.box.point_number - 1)
+            k_init = self.box.point_number // 2
+        else:
+            k_init = self.slider.value()
+
+        x, y, z = (np.arange(self.box.point_number) / self.box.point_number - 1 / 2) * self.box.box_size[:, None]
+        X, Y = np.meshgrid(x, y)
+        Z = intensity[:, :, int(k_init)].T
+        minValue = min(np.amin(intensity), 0.0)
+        maxValue = min(np.amax(intensity), 100.0)
+        print(maxValue)
+        levels = np.linspace(minValue, maxValue + 1, 30)
+        cf = ax.contourf(X, Y, Z, levels)
+
+        self.colorbar = self.canvas.figure.colorbar(cf)
+        z_val = z[k_init]
+        ax.set_title("Intensity, z = {:.2f}".format(z_val))
+        ax.set_xlabel("X, $\lambda$")
+        ax.set_ylabel("Y, $\lambda$")
+        if self.box.info:
+            ax.text(self.box.box_size[0] / 2, 1.05 * self.box.box_size[1] / 2, self.box.info, color='red')
+
+        self.canvas.draw()
+
+        def update(slider_val):
+            ax.clear()
+            z_val = z[slider_val]
+            ax.set_title("Intensity, z = {:.2f}".format(z_val))
+            Z = intensity[:, :, int(slider_val)].T
+            ax.contourf(X, Y, Z, levels)
+            if self.box.info:
+                ax.text(self.box.box_size[0] / 2, 1.05 * self.box.box_size[1] / 2, self.box.info, color='red')
+            self.canvas.draw()
+
+        self.slider.valueChanged.connect(update)
+
+    def plot_fourier_space_slices(self, intensity = None, mode="linear"):
         self.canvas.figure.clear()
         ax = self.canvas.figure.add_subplot(111)
         ax.set_aspect('equal')
@@ -204,18 +268,26 @@ class MainWindow(QMainWindow):
             self.canvas_layout.addWidget(self.slider)
             self.slider.setMinimum(0)
             self.slider.setMaximum(self.box.point_number - 1)
-            k_init = self.box.point_number / 2
+            k_init = self.box.point_number // 2
         elif self.slider:
             k_init = self.slider.value()
 
-        intensity = abs(self.box.intensity_fourier_space) * (self.box.box_size / self.box.point_number) ** 3
+        intensity = ((np.abs(self.box.intensity_fourier_space) *
+                     self.box.box_size[0] * self.box.box_size[1] * self.box.box_size[2] / self.box.point_number ** 3)
+                     if intensity is None else intensity)
 
-        fx = np.linspace(- self.box.point_number / self.box.box_size / 2.,
-                         (self.box.point_number - 1) / self.box.box_size / 2, self.box.point_number)
+        fx = np.linspace(- self.box.point_number / self.box.box_size[0] / 2.,
+                         (self.box.point_number - 1) / self.box.box_size[0] / 2, self.box.point_number)
 
-        Fx, Fy = np.meshgrid(fx, fx)
+        fy = np.linspace(- self.box.point_number / self.box.box_size[1] / 2.,
+                         (self.box.point_number - 1) / self.box.box_size[1] / 2, self.box.point_number)
 
-        Z = self.plotting_mode(intensity[:, :, int(k_init)].T, mode)
+        fz = np.linspace(- self.box.point_number / self.box.box_size[2] / 2.,
+                         (self.box.point_number - 1) / self.box.box_size[2] / 2, self.box.point_number)
+
+        Fx, Fy = np.meshgrid(fx, fy)
+
+        Z = self.plotting_mode(intensity[:, :, k_init].T, mode)
 
         minValue = min(np.amin(intensity), 0.0)
         maxValue = min(np.amax(intensity), 100.0)
@@ -224,20 +296,20 @@ class MainWindow(QMainWindow):
         cf = ax.contourf(Fx, Fy, Z, levels)
 
         self.colorbar = self.canvas.figure.colorbar(cf)
-        fz_val = k_init / self.box.box_size - self.box.point_number / (2 * self.box.box_size)
+        fz_val = fz[k_init]
 
         ax.set_title("Intensity, fz = {:.2f}".format(fz_val))
         ax.set_xlabel("Fx, $\\frac{1}{\lambda}$")
         ax.set_ylabel("Fy, $\\frac{1}{\lambda}$")
 
         if self.box.info:
-            ax.text(np.abs(fx[0]), 1.05 * np.abs(fx[0]), self.box.info, color='red')
+            ax.text(np.abs(fx[0]), 1.05 * np.abs(fy[0]), self.box.info, color='red')
 
         self.canvas.draw()
 
         def update(slider_val):
             ax.clear()
-            fz_val = slider_val / self.box.box_size - self.box.point_number / (2 * self.box.box_size)
+            fz_val = fz[slider_val]
             ax.set_title("Intensity, fz = {:.2f}".format(fz_val))
             ax.set_xlabel("Fx, $\\frac{1}{\lambda}$")
             ax.set_ylabel("Fy, $\\frac{1}{\lambda}$")
@@ -249,52 +321,11 @@ class MainWindow(QMainWindow):
 
         self.slider.valueChanged.connect(update)
 
-    def plot_intensity_slices(self):
-        self.canvas.figure.clear()
-        ax = self.canvas.figure.add_subplot(111)
-        ax.set_aspect('equal')
-
-        intensity = self.box.intensity.real
-
-        if not self.slider:
-            self.slider = QSlider(Qt.Horizontal)  # Horizontal slider
-            self.canvas_layout.addWidget(self.slider)
-            self.slider.setMinimum(0)
-            self.slider.setMaximum(self.box.point_number - 1)
-            k_init = self.box.point_number / 2
-        else:
-            k_init = self.slider.value()
-
-        values = (np.arange(self.box.point_number) / self.box.point_number - 1 / 2) * self.box.box_size
-        X, Y = np.meshgrid(values, values)
-        Z = intensity[:, :, int(k_init)].T
-        minValue = min(np.amin(intensity), 0.0)
-        maxValue = min(np.amax(intensity), 100.0)
-        print(maxValue)
-        levels = np.linspace(minValue, maxValue + 1, 30)
-        cf = ax.contourf(X, Y, Z, levels)
-
-        self.colorbar = self.canvas.figure.colorbar(cf)
-        z_val = (k_init / self.box.point_number - 1 / 2) * self.box.box_size
-        ax.set_title("Intensity, z = {:.2f}".format(z_val))
-        ax.set_xlabel("X, $\lambda$")
-        ax.set_ylabel("Y, $\lambda$")
-        if self.box.info:
-            ax.text(self.box.box_size / 2, 1.05 * self.box.box_size / 2, self.box.info, color='red')
-
-        self.canvas.draw()
-
-        def update(slider_val):
-            ax.clear()
-            z_val = (slider_val / self.box.point_number - 1 / 2) * self.box.box_size
-            ax.set_title("Intensity, z = {:.2f}".format(z_val))
-            Z = intensity[:, :, int(slider_val)].T
-            ax.contourf(X, Y, Z, levels)
-            if self.box.info:
-                ax.text(self.box.box_size / 2, 1.05 * self.box.box_size / 2, self.box.info, color='red')
-            self.canvas.draw()
-
-        self.slider.valueChanged.connect(update)
+    def plot_numerically_approximated_intensity(self):
+        self.plot_intensity_slices(intensity=self.box.numerically_approximated_intensity)
+    def plot_numerically_approximated_intensity_fourier_space(self):
+        intensity = np.abs(self.box.numerically_approximated_intensity_fourier_space) * self.box.box_volume / self.box.point_number**3
+        self.plot_fourier_space_slices(intensity = intensity)
 
     def compute_and_plot_from_electric_field(self):
         self.box.compute_electric_field()
@@ -309,6 +340,8 @@ class MainWindow(QMainWindow):
         self.box.compute_intensity_fourier_space()
         self.plot_fourier_space_slices()
 
+    def compute_numerically_approximated_intensities(self):
+        self.box.compute_intensity_and_spacial_waves_numerically()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

Illumination.py
@@ -1,17 +1,25 @@
 import numpy as np
 import VectorOperations
+import matplotlib.pyplot as plt
+import stattools
 class Illumination:
-    def __init__(self, intensity_plane_waves_dict, spacial_shifts=(np.array((0, 0, 0)), ), M_r = 1):
-        self.angles = np.arange(0, 2 * np.pi, 2 * np.pi / M_r)
-        self._spacial_shifts = spacial_shifts
-        self._M_r = M_r
-        self.M_t = len(self.spacial_shifts)
+    def __init__(self, intensity_plane_waves_dict,  Mr = 1):
+        self.angles = np.arange(0, 2 * np.pi, 2 * np.pi / Mr)
+        self._spacial_shifts = np.array((0, 0, 0))
+        self._Mr = Mr
+        self.Mt = len(self.spacial_shifts)
         self.waves = intensity_plane_waves_dict
 
     @classmethod
-    def init_from_list(cls, intensity_plane_waves_list, base_vector_lengths, spacial_shifts=(np.array((0, 0, 0))), M_r = 1):
+    def init_from_list(cls, intensity_plane_waves_list, base_vector_lengths, Mr = 1):
         intensity_plane_waves_dict = cls.index_frequencies(intensity_plane_waves_list, base_vector_lengths)
-        return cls(intensity_plane_waves_dict, spacial_shifts = spacial_shifts, M_r = M_r)
+        return cls(intensity_plane_waves_dict, Mr = Mr)
+    
+    @classmethod
+    def init_from_numerical_intensity_fourier_domain(cls, numerical_intensity_fourier_domain, axes, Mr = 1):
+        fourier_peaks, amplitudes = stattools.estimate_localized_peaks(numerical_intensity_fourier_domain, axes)
+
+
     @staticmethod
     def index_frequencies(waves_list, base_vector_lengths):
         intensity_plane_waves_dict = {}
@@ -23,13 +31,13 @@ class Illumination:
         return intensity_plane_waves_dict
 
     @property
-    def M_r(self):
-        return self._M_r
+    def Mr(self):
+        return self._Mr
 
-    @M_r.setter
-    def M_r(self, new_M_r):
-        self.M_r = new_M_r
-        self.angles = np.arange(0, 2 * np.pi, 2 * np.pi / new_M_r)
+    @Mr.setter
+    def Mr(self, new_Mr):
+        self.Mr = new_Mr
+        self.angles = np.arange(0, 2 * np.pi, 2 * np.pi / new_Mr)
 
     @property
     def spacial_shifts(self):
@@ -38,7 +46,7 @@ class Illumination:
     @spacial_shifts.setter
     def spacial_shifts(self, new_spacial_shifts):
         self._spacial_shifts = new_spacial_shifts
-        self.M_t = len(new_spacial_shifts)
+        self.Mt = len(new_spacial_shifts)
 
     def get_wavevectors(self):
         wavevectors = []
@@ -48,3 +56,4 @@ class Illumination:
                     spacial_wave.wavevector, np.array((0, 0, 1)), angle)
                 wavevectors.append(wavevector)
         return wavevectors
+    

config/IlluminationConfigurations.py
@@ -96,14 +96,14 @@ seven_waves_list = [
     Sources.IntensityPlaneWave(-1j * b/norm, 0, np.array((k1, 0, k2))),
     Sources.IntensityPlaneWave(-1j * b/norm, 0, np.array((k1, 0, -k2))),
 
-    Sources.IntensityPlaneWave((2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, 3**0.5/2 * k1, -k2))),
-    Sources.IntensityPlaneWave((-2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, -3**0.5/2 * k1, -k2))),
-    Sources.IntensityPlaneWave((2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, 3**0.5/2 * k1, -k2))),
-    Sources.IntensityPlaneWave((-2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, -3**0.5/2 * k1, -k2))),
-    Sources.IntensityPlaneWave((-2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, 3**0.5/2 * k1, k2))),
-    Sources.IntensityPlaneWave((2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, -3**0.5/2 * k1, k2))),
-    Sources.IntensityPlaneWave((-2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, 3**0.5/2 * k1, k2))),
-    Sources.IntensityPlaneWave((2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, -3**0.5/2 * k1, k2))),
+    Sources.IntensityPlaneWave((2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, 3**0.5/2 * k1, k2))),
+    Sources.IntensityPlaneWave((-2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, -3**0.5/2 * k1, k2))),
+    Sources.IntensityPlaneWave((2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, 3**0.5/2 * k1, k2))),
+    Sources.IntensityPlaneWave((-2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, -3**0.5/2 * k1, k2))),
+    Sources.IntensityPlaneWave((-2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, 3**0.5/2 * k1, -k2))),
+    Sources.IntensityPlaneWave((2*3**0.5 - 2j)/4 * b/norm, 0, np.array((k1/2, -3**0.5/2 * k1, -k2))),
+    Sources.IntensityPlaneWave((-2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, 3**0.5/2 * k1, -k2))),
+    Sources.IntensityPlaneWave((2*3**0.5 + 2j)/4 * b/norm, 0, np.array((-k1/2, -3**0.5/2 * k1, -k2))),
 ]
 seven_waves_illumination = Illumination.index_frequencies(seven_waves_list, (k1/2, 3**0.5/2 * k1, k2))
 

stattools.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.optimize import minimize
 def average_ring(array, axes=None):
     if not axes:
         ax1, ax2 = np.arange(array.shape[0]) - array.shape[0]/ 2, np.arange(array.shape[1]) - array.shape[1] / 2
@@ -25,3 +25,60 @@ def average_ring(array, axes=None):
     ring_numbers = np.diff(circle_point_numbers)
     ring_averages = ring_sums / ring_numbers
     return ring_averages[::-1]
+
+def estimate_localized_peaks(array, axes):
+    if len(array.shape) != 3:
+        raise ValueError("Estimation is only available for 3d arrays now")
+    array_abs = np.abs(array)
+    max_value = np.amax(array_abs)
+    array = np.where(array_abs > max_value * 10**-4, array, 0)
+    array_abs = np.where(array_abs > max_value * 10**-2, array_abs, 0)
+    maxima_indices = np.array(np.where((array_abs[1:-1, 1:-1, 1:-1] > array_abs[0:-2, 1:-1, 1:-1]) * (array_abs[1:-1, 1:-1, 1:-1] > array_abs[2:, 1:-1, 1:-1]) *
+                                       (array_abs[1:-1, 1:-1, 1:-1] > array_abs[1:-1, 0:-2, 1:-1]) * (array_abs[1:-1, 1:-1, 1:-1] > array_abs[1:-1, 2:, 1:-1]) *
+                                       (array_abs[1:-1, 1:-1, 1:-1] > array_abs[1:-1, 1:-1, 0:-2]) * (array_abs[1:-1, 1:-1, 1:-1] > array_abs[1:-1, 1:-1, 2:]))) + 1
+    maxima_indices = list(zip(maxima_indices[0], maxima_indices[1], maxima_indices[2]))
+    fourier_peaks, std = gaussian_maxima_fitting(array_abs, axes, maxima_indices)
+    amplitudes = np.zeros(len(maxima_indices), dtype=np.complex128)
+    distances2 = np.zeros((len(fourier_peaks), axes[0].size, axes[1].size, axes[2].size))
+    for i in range(len(fourier_peaks)):
+        xdist2 = (axes[0] - fourier_peaks[i][0])**2
+        ydist2 = (axes[1] - fourier_peaks[i][1])**2
+        zdist2 = (axes[2] - fourier_peaks[i][2])**2
+        Xdist2, Ydist2, Zdist2 = np.meshgrid(ydist2, xdist2, zdist2)
+        Rdist2 = Xdist2 + Ydist2 + Zdist2
+        a = np.argmin(Rdist2)
+        distances2[i] = Rdist2
+    minimal_distance_array = np.min(distances2, axis=0)
+    nearest_neighbor_array = np.zeros((len(axes[0]), len(axes[1]), len(axes[2])))
+    for i in range(1, len(fourier_peaks)):
+        nearest_neighbor_array = np.where(distances2[i] == minimal_distance_array, i, nearest_neighbor_array)
+    for i in range(0, len(fourier_peaks)):
+        amplitudes[i] = np.sum(array[nearest_neighbor_array == i])
+    normalization = np.amax(np.abs(amplitudes))
+    amplitudes /= normalization
+    return fourier_peaks, amplitudes
+
+def gaussian_maxima_fitting(array, axes, maxima_indices, size=5):
+    maxima_fitted = []
+    std = []
+    for index in maxima_indices:
+        fitted_values = array[index[0]-size//2:index[0] + size//2 + 1,
+                              index[1]-size//2:index[1] + size//2 + 1,
+                              index[2]-size//2:index[2] + size//2 + 1]
+
+        x, y, z = (axes[0][index[0]-size//2:index[0] + size//2 + 1],
+                   axes[1][index[1]-size//2:index[1] + size//2 + 1],
+                   axes[2][index[2]-size//2:index[2] + size//2 + 1])
+        X, Y, Z = np.meshgrid(x, y, z)
+        n = np.sum(fitted_values)
+        x0 = np.sum(X * fitted_values) / n
+        y0 = np.sum(Y * fitted_values) / n
+        z0 = np.sum(Z * fitted_values) / n
+        sigma = (1 / (2 * n) * np.sum(fitted_values * ((X - x0) ** 2 + (Y - y0) ** 2)) + (Z - z0)**2) ** 0.5
+        maxima_fitted.append((x0, y0, z0))
+        std.append(sigma)
+
+    return np.array(maxima_fitted), np.array(std)
+
+
+

unittests/test_illumination.py
@@ -1,5 +1,10 @@
 import unittest
 from config.IlluminationConfigurations import *
+from Box import Box
+from Sources import PlaneWave, IntensityPlaneWave
+from VectorOperations import VectorOperations
+import matplotlib.pyplot as plt
+import stattools
 class TestIllumination(unittest.TestCase):
     def test_index_waves(self):
         sources = [
@@ -33,6 +38,47 @@ class TestIllumination(unittest.TestCase):
         ]
         five_waves_dict = Illumination.index_frequencies(sources, base_vector_lengths=(k1, k1, k2))
         assert (five_waves_dict.keys() == s_polarized_waves.keys())
-
     def test_seven_waves_illumination(self):
         print(seven_waves_illumination)
+
+    def test_numerical_peaks_search(self):
+        theta = np.pi / 4
+        b = 1
+        k = 2 * np.pi
+        k1 = k * np.sin(theta)
+        k2 = k * (np.cos(theta) - 1)
+        vec_x = np.array((k/2 * np.sin(theta), 0, k * np.cos(theta)))
+        vec_mx = np.array((-k * np.sin(theta), 0, k * np.cos(theta)))
+        ax_z = np.array((0, 0, 1))
+
+        sources = [
+
+            PlaneWave(0, b, 0, 0, vec_x),
+            PlaneWave(0, b, 0, 0, vec_mx),
+            PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z,  2 * np.pi/3)),
+            PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_mx, ax_z,  2 * np.pi/3)),
+            PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, 4 * np.pi / 3)),
+            PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_mx, ax_z, 4 * np.pi / 3)),
+
+            PlaneWave(1, 1j * 1, 0, 0, np.array((0, 0, k))),
+        ]
+        box = Box(sources, 15, 100)
+        axes = (box.grid[:, 0, 0, 0], box.grid[0, :, 0, 1], box.grid[0, 0, :, 2])
+        box.compute_electric_field()
+        box.compute_intensity_from_electric_field()
+        box.compute_intensity_fourier_space()
+        fourier_peaks, amplitudes = stattools.estimate_localized_peaks(box.intensity_fourier_space, axes)
+        print(len(fourier_peaks))
+        intensity_sources_discrete = []
+        for fourier_peak, amplitude in zip(fourier_peaks, amplitudes):
+            intensity_sources_discrete.append(IntensityPlaneWave(amplitude, 0, 2 * np.pi * np.array(fourier_peak)))
+            print(fourier_peak*np.pi * 2)
+        res = stattools.find_optimal_base_vectors(fourier_peaks)
+        print(fourier_peaks[:10])
+        print(res)
+        box2 = Box(intensity_sources_discrete, (20, 20, 80), 100)
+        box2.compute_intensity_from_spacial_waves()
+        box2.compute_intensity_fourier_space()
+        # plt.imshow(np.abs(box2.intensity_fourier_space[:, :, 20]))
+        # plt.show()
+