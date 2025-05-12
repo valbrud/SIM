Fixed numerical bugs

Apodization.py
@@ -148,7 +148,7 @@ class AutoconvolutuionApodizationSIM2D(AutoconvolutionApodizationSIM):
         super().__init__(optical_system, illumination)
 
     def _compute_ideal_transfer_functions(self, **kwargs):
-        ideal_pupil_function = np.where(np.abs(self.pupil_function) > 10**-1 * np.amax(self.pupil_function), 1, 0)
+        ideal_pupil_function = np.where(np.abs(self.pupil_function) > 10**-1 * np.amax(self.pupil_function), 1., 0)
         ideal_pupil_function_ift = wrappers.wrapped_ifftn(ideal_pupil_function)
         
         grid = self._optical_system.x_grid
@@ -166,6 +166,7 @@ class AutoconvolutuionApodizationSIM2D(AutoconvolutionApodizationSIM):
 
         self._ideal_ctf = ideal_pupil_function
         self._ideal_otf = np.flip(scipy.signal.convolve(self._ideal_ctf, np.flip(self._ideal_ctf).conjugate(), mode='same'))
+        self._ideal_otf /= np.amax(self._ideal_otf)
         self._ideal_psf = wrappers.wrapped_ifftn(self._ideal_otf).real
         self._ideal_psf /= np.sum(self._ideal_psf)
     

Illumination.py
@@ -123,7 +123,7 @@ class PlaneWavesSIM(Illumination, PeriodicStructure):
 
         self.sim_indices, self.projected_wavevectors = self.get_wavevectors_projected(0)
 
-        self.phase_matrix = {}
+        self._phase_matrix = {}
         self.compute_phase_matrix()
 
         self.electric_field_plane_waves = []
@@ -289,14 +289,17 @@ class PlaneWavesSIM(Illumination, PeriodicStructure):
         pass
 
     def normalize_spatial_waves(self):
-        zero_index =  tuple([0] * len(self.dimensions))
-        if not zero_index in self.harmonics.keys():
-            return AttributeError("Zero wavevector is not found! No constant power in the illumination!")
-        norm = self.harmonics[zero_index].amplitude * self.Mt * self.Mr
-        for spatial_wave in self.harmonics.values():
-            spatial_wave.amplitude = spatial_wave.amplitude * np.exp(1j * spatial_wave.phase)
-            spatial_wave.phase = 0
-            spatial_wave.amplitude /= norm
+        for r in range(self.Mr):
+            zero_index = (r, tuple([0] * len(self.dimensions)))
+            if not zero_index in self.harmonics.keys():
+                raise AttributeError("Zero wavevector is not found! No constant power in the illumination!")
+            norm = self.harmonics[zero_index].amplitude * self.Mt * self.Mr
+            for harmonic in self.harmonics.keys():
+                if harmonic[0] == r:
+                    spatial_wave = self.harmonics[harmonic]
+                    spatial_wave.amplitude = spatial_wave.amplitude * np.exp(1j * spatial_wave.phase)
+                    spatial_wave.phase = 0
+                    spatial_wave.amplitude /= norm
 
 
     def compute_expanded_lattice(self, r=0, ignore_projected_dimensions=True) -> set[tuple[int, int]]:
@@ -471,8 +474,8 @@ class PlaneWavesSIM(Illumination, PeriodicStructure):
                     phase_shifted = np.transpose(np.exp(1j * np.einsum('ijkl,l ->ijk', grid, wavevector)), axes=(1, 0, 2)) * kernel
                 effective_kernel += amplitude * phase_shifted
 
-            effective_kernels[index] = effective_kernel
-            effective_kernels_ft[index] = wrappers.wrapped_fftn(effective_kernel)
+            effective_kernels[sim_index] = effective_kernel
+            effective_kernels_ft[sim_index] = wrappers.wrapped_fftn(effective_kernel)
         return effective_kernels, effective_kernels_ft
 
     def get_phase_modulation_patterns(self, coordinates):

SSNRCalculator.py
@@ -214,12 +214,13 @@ class SSNRSIM(SSNRBase):
                  readout_noise_variance:float=0,
                  effective_otfs={},
                  effective_kernels_ft={},
-                 save_memory=False
+                 save_memory=False, 
+                 illumination_reconstruction=None
                  ):
-        if not isinstance(illumination, PlaneWavesSIM):
-            raise AttributeError("Illumination data is not of SIM type!")
+        
         super().__init__(optical_system)
         self._illumination = illumination
+        self._illumination_reconstruction = illumination_reconstruction if not illumination_reconstruction is None else illumination
         self.vj = None
         self.dj = None
 
@@ -244,6 +245,8 @@ class SSNRSIM(SSNRBase):
         if not self.effective_kernels_ft:
             if not kernel is None:
                 self.kernel = kernel
+            else:
+                self.kernel = self.optical_system.psf
 
         self._compute_ssnri()
 
@@ -264,11 +267,25 @@ class SSNRSIM(SSNRBase):
 
     @illumination.setter
     def illumination(self, new_illumination):
+        if self._illumination is self._illumination_reconstruction:
+            self._illumination_reconstruction = None 
         self._illumination = new_illumination
         self.effective_otfs = {}
         self._compute_effective_otfs()
-        if self.kernel:
-            self._compute_effective_kernels_ft()
+        if self.illumination_reconstruction is None:
+            self.illumination_reconstruction = new_illumination
+        else:
+            self._compute_ssnri()
+
+    @property 
+    def illumination_reconstruction(self):
+        return self._illumination_reconstruction
+    
+    @illumination_reconstruction.setter
+    def illumination_reconstruction(self, new_illumination_reconstruction):
+        self._illumination_reconstruction = new_illumination_reconstruction
+        self.effective_kernels_ft = {}
+        self._compute_effective_kernels_ft()
         self._compute_ssnri()
 
     @property
@@ -280,8 +297,8 @@ class SSNRSIM(SSNRBase):
         shape = np.array(kernel_new.shape, dtype=np.int32)
         otf_shape = np.array(self.optical_system.otf.shape, dtype=np.int32)
 
-        if ((shape % 2) == 0).any():
-            raise ValueError("Size of the kernel must be odd!")
+        # if ((shape % 2) == 0).any():
+        #     raise ValueError("Size of the kernel must be odd!")
 
         if (shape > otf_shape).any():
             raise ValueError("Size of the kernel is bigger than of the PSF!")
@@ -320,17 +337,15 @@ class SSNRSIM(SSNRBase):
             self.otf_sim += np.abs(self.effective_otfs[m])
 
     def _compute_effective_kernels_ft(self):
-        _, self.effective_kernels_ft = self.illumination.compute_effective_kernels(self.kernel, self.optical_system.psf_coordinates)
+        if np.isclose(self.kernel, self.optical_system.psf).all() and self.illumination_reconstruction is self.illumination:
+            self.effective_kernels_ft = self.effective_otfs
+        else:
+            _, self.effective_kernels_ft = self.illumination_reconstruction.compute_effective_kernels(self.kernel, self.optical_system.psf_coordinates)
 
     def _compute_Dj(self):
-        if not self.effective_kernels_ft:
-            effective_kernels_ft = self.effective_otfs
-        else:
-            effective_kernels_ft = self.effective_kernels_ft
-
         d_j = np.zeros(self.optical_system.otf.shape, dtype=np.complex128)
         for m in self.effective_otfs.keys():
-            d_j += self.effective_otfs[m] * effective_kernels_ft[m].conjugate()
+            d_j += self.effective_otfs[m] * self.effective_kernels_ft[m].conjugate()
         d_j *= self.illumination.Mt
         # plt.title("Dj")
         # plt.imshow(np.log(1 + 10**8 * np.abs(d_j)[:, :, 50]))
@@ -338,11 +353,6 @@ class SSNRSIM(SSNRBase):
         return np.abs(d_j)
 
     def _compute_Vj(self):
-        if not self.effective_kernels_ft:
-            effective_kernels_ft = self.effective_otfs
-        else:
-            effective_kernels_ft = self.effective_kernels_ft
-
         center = np.array(self.optical_system.otf.shape, dtype=np.int32) // 2
         v_j = np.zeros(self.optical_system.otf.shape, dtype=np.complex128)
 
@@ -357,8 +367,8 @@ class SSNRSIM(SSNRBase):
                 if (r, m21) not in self.illumination.rearranged_indices:
                     continue
                 idx_diff = (r, m21)
-                otf1 = effective_kernels_ft[idx1]
-                otf2 = effective_kernels_ft[idx2]
+                otf1 = self.effective_kernels_ft[idx1]
+                otf2 = self.effective_kernels_ft[idx2]
                 otf3 = self.effective_otfs[idx_diff][*center]
                 term = otf1 * otf2.conjugate() * otf3
                 v_j += term
@@ -390,7 +400,7 @@ class SSNRSIM(SSNRBase):
     def compute_analytic_ssnri_volume(self, factor=10, volume_element=1):
         g2 = np.sum(self.optical_system.otf * self.optical_system.otf.conjugate()).real
         g0 = np.abs(np.amax(self.optical_system.otf))
-        weights = np.array([wave.amplitude for wave in self.illumination.waves.values()])
+        weights = np.array([harmonic.amplitude for harmonic in self.illumination.harmonics.values()])
         weighted2sum = np.sum(weights * weights.conjugate()).real
         volume = ((self.illumination.Mt * self.illumination.Mr) ** 2 * weighted2sum * g2 /
                   g0 * volume_element * factor)
@@ -406,7 +416,7 @@ class SSNRSIM(SSNRBase):
         g2 = np.sum(self.optical_system.otf * self.optical_system.otf.conjugate()).real
         g4 = np.sum(self.optical_system.otf ** 2 * self.optical_system.otf.conjugate() ** 2).real
         g0 = np.abs(np.amax(self.optical_system.otf))
-        weights = np.array([wave.amplitude for wave in self.illumination.waves.values()])
+        weights = np.array([harmonic.amplitude for harmonic in self.illumination.harmonics.values()])
         weighted2sum = np.sum(weights * weights.conjugate()).real
         weighted4sum = np.sum(weights ** 2 * weights.conjugate() ** 2).real
         total = ((self.illumination.Mt * self.illumination.Mr) ** 2 * weighted4sum * g4 /
@@ -434,7 +444,8 @@ class SSNRSIM(SSNRBase):
         return fR + fI
 
     def compute_ssnr_waterline_measure(self, factor=10):
-        ssnr_widefield = SSNRPointScanning(self.optical_system).ssnr
+        Widefield = SSNRWidefield2D if self.dimensionality == 2 else SSNRWidefield3D
+        ssnr_widefield = Widefield(self.optical_system).ssnri
         diff = np.sum(self.ssnri - ssnr_widefield).real
         upper_estimate = np.abs(np.amax(self.ssnri - ssnr_widefield))
         noise_level = 10 ** -10 * np.abs(np.amax(self.ssnri))
@@ -446,12 +457,21 @@ class SSNRSIM(SSNRBase):
 
 class SSNRSIM2D(SSNRSIM):
     dimensionality = 2
-    def __init__(self, illumination: IlluminationPlaneWaves2D, optical_system, kernel=None, readout_noise_variance=0, save_memory=False):
+    def __init__(self,
+                 illumination: IlluminationPlaneWaves2D,
+                 optical_system,
+                 kernel=None,
+                 readout_noise_variance=0,
+                 save_memory=False,
+                 illumination_reconstruction=None):
         if not isinstance(illumination, IlluminationPlaneWaves2D):
             raise AttributeError("Illumination data is not of the valid dimension!")
         if not isinstance(optical_system, OpticalSystems.OpticalSystem2D):
             raise AttributeError("Optical system data is not of the valid dimension!")
-        super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory)
+        if not isinstance(illumination_reconstruction, IlluminationPlaneWaves2D) and not illumination_reconstruction is None:
+            raise AttributeError("Illumination reconstruction data is not of the valid dimension!")
+        
+        super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory, illumination_reconstruction=illumination_reconstruction)
 
     def plot_effective_kernel_and_otf(self):
         Nx, Ny = self.optical_system.otf.shape
@@ -480,9 +500,17 @@ class SSNRSIM2D(SSNRSIM):
 
 class SSNRSIM3D(SSNRSIM):
     dimensionality = 3
-    def __init__(self, illumination, optical_system, kernel=None, readout_noise_variance=0, save_memory=False):
+    def __init__(self,
+                 illumination,
+                 optical_system,
+                 kernel=None,
+                 readout_noise_variance=0,
+                 save_memory=False, 
+                 illumination_reconstruction=None):
         if not isinstance(illumination, IlluminationPlaneWaves3D):
             raise AttributeError("Illumination data is not of the valid dimension!")
         if not isinstance(optical_system, OpticalSystems.OpticalSystem3D):
             raise AttributeError("Optical system data is not of the valid dimension!")
-        super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory)
+        if not isinstance(illumination_reconstruction, IlluminationPlaneWaves3D) and not illumination_reconstruction is None:
+            raise AttributeError("Illumination reconstruction data is not of the valid dimension!")
+        super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory, illumination_reconstruction=illumination_reconstruction)

WienerFiltering.py
@@ -43,7 +43,7 @@ def filter_constant(image_ft, otf, w):
     
     return filtered, w
 
-def filter_flat_noise(image_ft, otf, ssnr_calculator): 
+def filter_flat_noise(image_ft, ssnr_calculator): 
     """
     Applies a Winer filtering (deconvolution) procedure with a regularization filter
     set up to ensure the flat noise. 
@@ -60,16 +60,16 @@ def filter_flat_noise(image_ft, otf, ssnr_calculator):
         raise ValueError("The image and SSNR calculator dimensions do not match.")
     
     w = ssnr_calculator.vj ** 0.5 - ssnr_calculator.dj
-    filtered = image_ft * otf.conjugate() / (otf * otf.conjugate() + w)
+    # filtered = image_ft * otf.conjugate() / (otf * otf.conjugate() + w)
+    filtered = image_ft * ssnr_calculator.dj.conjugate() / (ssnr_calculator.dj * ssnr_calculator.dj.conjugate() + w)
     return filtered, w
 
 def filter_true_wiener(image_ft,
-                    otf,
                     ssnr_calculator,
                     vja = None, 
                     dja = None,
                     average='rings',
-                    numeric_noise=10**-12):    
+                    numeric_noise=10**-4):    
     """
     Applies a Winer filtering (deconvolution) procedure with a regularization filter
     set up to ensure the best contrast (True Wiener). In the (realistic) case of the unknown
@@ -120,9 +120,12 @@ def filter_true_wiener(image_ft,
     ssnr = ((obj2a - vja * f0 - image_ft.size * ssnr_calculator.readout_noise_variance**2 * dja) /
                     (vja * f0 + image_ft.size * ssnr_calculator.readout_noise_variance**2 * dja)).real
     ssnr = np.nan_to_num(ssnr)
-
+    # plt.imshow(np.log1p(10 ** 8 * np.abs(ssnr)))
+    plt.imshow(np.log1p(10**8 * np.abs(np.where(ssnr_calculator.dj > numeric_noise, ssnr_calculator.dj, 0))))
+    plt.show()
     w = (dja + 10**3 * numeric_noise)/(ssnr + numeric_noise)
-    filtered = image_ft * otf.conjugate() / (otf * otf.conjugate() + w)
-
-    return filtered, w
+    # filtered = image_ft * otf.conjugate() / (otf * otf.conjugate() + w)
+    filtered = image_ft * ssnr_calculator.dj.conjugate() / (ssnr_calculator.dj * ssnr_calculator.dj.conjugate() + w)
+    filtered = np.where(ssnr_calculator.dj > numeric_noise, filtered, 0)
+    return filtered, w, ssnr
 

config/BFPConfigurations.py
@@ -30,17 +30,14 @@ class BFPConfiguration:
         b = strength_oblique
         a0 = p ** 2 + 2 * b ** 2
 
-        base_vector_kx = k1
-        base_vector_ky = k1  # All ky indices should be zero for a plane illumination
-        base_vector_kz = k2  # We do not really care about a z index. Hopefully
-        base_vectors = (base_vector_kx, base_vector_ky, base_vector_kz)
-
         sources = [
             Sources.PlaneWave(0, b / a0 ** 0.5, 0, 0, np.array((k1, 0, k2))),
             Sources.PlaneWave(0, -b / a0 ** 0.5, 0, 0, np.array((-k1, 0, k2))),
             Sources.PlaneWave(0, p / a0 ** 0.5, 0, 0, np.array((1e-12, 0, 1e-9))),
 
         ]
+
+        base_vectors = (k1, k1, k2)
         illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
                                                                       base_vectors,
                                                                       dimensions=(1, 1, 0), 
@@ -58,8 +55,8 @@ class BFPConfiguration:
         k2 = self.k * (np.cos(theta) - 1)
 
         base_vector_kx = k1
-        base_vector_ky = k1  # All ky indices should be zero for a plane illumination
-        base_vector_kz = k2  # We do not really care about a z index. Hopefully
+        base_vector_ky = k1 
+        base_vector_kz = k2  
         base_vectors = (base_vector_kx, base_vector_ky, base_vector_kz)
         # p = strength_s_normal
         p = strength_s_normal
@@ -93,13 +90,15 @@ class BFPConfiguration:
         b = strength_oblique
         a0 = (2 * p ** 2 + 4 * b ** 2)
 
-        square_plane_waves = {
+        sources = {
             Sources.PlaneWave(0, b / a0 ** 0.5, 0, 0, np.array((k1, 0, self.k * np.cos(theta)))),
             Sources.PlaneWave(0, b / a0 ** 0.5, 0, 0, np.array((-k1, 0, self.k * np.cos(theta)))),
             Sources.PlaneWave(0, b / a0 ** 0.5, 0, 0, np.array((0, k1, self.k * np.cos(theta)))),
             Sources.PlaneWave(0, b / a0 ** 0.5, 0, 0, np.array((0, -k1, self.k * np.cos(theta)))),
             Sources.PlaneWave(p / a0 ** 0.5 * np.exp(1j * phase_shift), 1j * p / a0 ** 0.5 * np.exp(1j * phase_shift), 0, 0, np.array((0, 0, self.k))),
         }
+        
+        ## The commented lists are for independent check of sources in case of the necessity during debugging or analytical comparison. 
 
         # s_polarized_waves = {
         #     (0, 0, 0)  : Sources.IntensityHarmonic3D(a0, 0, np.array((0, 0, 0))),
@@ -120,12 +119,14 @@ class BFPConfiguration:
         #     (0, -1, -1): Sources.IntensityHarmonic3D((-1 * p * b), 0, np.array((0, -k1, -k2)))
         # }
 
-        square_intensity_waves = IlluminationPlaneWaves3D.find_ipw_from_pw(square_plane_waves)
-
-        illumination =  IlluminationPlaneWaves3D.init_from_list(square_intensity_waves, (k1, k1, k2))
+        base_vectors = (k1, k1, k2)
+        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
+                                                                      base_vectors,
+                                                                      dimensions=(1, 1, 0), 
+                                                                      Mr = 1, 
+                                                                      store_plane_waves=True)
         illumination.Mt = Mt
         illumination.normalize_spatial_waves()
-
         return illumination
 
     def get_4_circular_oblique_waves_and_circular_normal(self, angle_oblique, strength_s_oblique, strength_s_normal=1, Mt=1, phase_shift=0):
@@ -138,13 +139,15 @@ class BFPConfiguration:
         b = strength_s_oblique
 
         a0 = (2 * p ** 2 + 8 * b ** 2)
-        circular_plane_waves = {
+        sources = {
             Sources.PlaneWave(b / a0 ** 0.5, 1j * b / a0 ** 0.5, 0, 0, np.array((k1, 0, self.k * np.cos(theta)))),
             Sources.PlaneWave(b / a0 ** 0.5, 1j * b / a0 ** 0.5, 0, 0, np.array((-k1, 0, self.k * np.cos(theta)))),
             Sources.PlaneWave(b / a0 ** 0.5, 1j * b / a0 ** 0.5, 0, 0, np.array((0, k1, self.k * np.cos(theta)))),
             Sources.PlaneWave(b / a0 ** 0.5, 1j * b / a0 ** 0.5, 0, 0, np.array((0, -k1, self.k * np.cos(theta)))),
             Sources.PlaneWave(p / a0 ** 0.5 * np.exp(1j * phase_shift), 1j * p / a0 ** 0.5 * np.exp(1j * phase_shift), 0, 0, np.array((0, 0, self.k))),
         }
+
+        ## The commented lists are for independent check of sources in case of the necessity during debugging or analytical comparison. 
 
         # circular_plane_waves = {
         #     Sources.PlaneWave(b/a0**0.5, 1j * b/a0**0.5, 0, 0, np.array((k1, 0, k2))),
@@ -153,6 +156,7 @@ class BFPConfiguration:
         #     Sources.PlaneWave(b/a0**0.5, 1j * b/a0**0.5, 0, 0, np.array((0, -k1, k2))),
         #     Sources.PlaneWave(p/a0**0.5 * np.exp(1j * phase_shift), 1j * p/a0**0.5 * np.exp(1j * phase_shift), 0, 0, np.array((0, 0, 0))),
         # }
+
         # circular_intensity_waves = {
         #     (1, 1, 0)  : Sources.IntensityHarmonic3D(2 * b ** 2 * np.sin(theta) ** 2, 0, np.array((k1, k1, 0))),
         #     (-1, 1, 0) : Sources.IntensityHarmonic3D(2 * b ** 2 * np.sin(theta) ** 2, 0, np.array((-k1, k1, 0))),
@@ -176,8 +180,10 @@ class BFPConfiguration:
         #
         #     (0, 0, 0)  : Sources.IntensityHarmonic3D(a0, 0, np.array((0, 0, 0)))
         # }
+
+
         base_vectors = (k1, k1, k2)
-        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(circular_plane_waves,
+        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
                                                                       base_vectors,
                                                                       dimensions=(1, 1, 0), 
                                                                       Mr = 1, 
@@ -197,7 +203,7 @@ class BFPConfiguration:
 
         a0 = 2 * p ** 2 + 6 * b ** 2
 
-        seven_waves_list = [
+        sources = [
             Sources.IntensityHarmonic3D(-b ** 2, 0, np.array((2 * k1, 0, 0))),
             Sources.IntensityHarmonic3D(-b ** 2, 0, np.array((-2 * k1, 0, 0))),
             Sources.IntensityHarmonic3D(-b ** 2, 0, np.array((-k1, 3 ** 0.5 * k1, 0))),
@@ -238,12 +244,15 @@ class BFPConfiguration:
             Sources.IntensityHarmonic3D((2 * 3 ** 0.5 + 2j) / 4 * b * p, 0, np.array((-k1 / 2, -3 ** 0.5 / 2 * k1, -k2))),
         ]
 
-        illumination =  IlluminationPlaneWaves3D.init_from_list(seven_waves_list, (k1 / 2, 3 ** 0.5 / 2 * k1, k2))
+        base_vectors = (k1 / 2, 3 ** 0.5 / 2 * k1, k2)
+        illumination = IlluminationPlaneWaves3D.init_from_list(sources,
+                                                                      base_vectors,
+                                                                      dimensions=(1, 1, 0), 
+                                                                      Mr = 1)
         illumination.Mt = Mt
         illumination.normalize_spatial_waves()
-
         return illumination
-
+    
 
     def get_4_s_oblique_waves_at_2_angles_and_one_normal_s_wave(self, angle1, k_ratio, strength1, strength2, strength_normal=1, Mr=3, Mt=1):
 
@@ -264,7 +273,7 @@ class BFPConfiguration:
 
         a0 = p ** 2 + 2 * b ** 2 + 2 * c ** 2
 
-        five_waves_2z_illumination = [
+        sources = [
             Sources.PlaneWave(0, b, 0, 0, vec_x),
             Sources.PlaneWave(0, c, 0, 0, vec_mx),
             Sources.PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, np.pi)),
@@ -272,11 +281,13 @@ class BFPConfiguration:
             Sources.PlaneWave(0, 1 * p, 0, 0, np.array((10 ** (-10), 0, self.k))),
         ]
 
-        five_waves_2z_illumination =  IlluminationPlaneWaves3D.find_ipw_from_pw(five_waves_2z_illumination)
-        illumination =  IlluminationPlaneWaves3D.init_from_list(five_waves_2z_illumination, base_vectors, Mr)
+        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
+                                                                      base_vectors,
+                                                                      dimensions=(1, 1, 0), 
+                                                                      Mr = 1, 
+                                                                      store_plane_waves=True)
         illumination.Mt = Mt
         illumination.normalize_spatial_waves()
-
         return illumination
 
     def get_two_oblique_triangles_and_one_normal_wave(self, angle1, k_ratio, strength1, strength2,
@@ -298,7 +309,7 @@ class BFPConfiguration:
         vec_mx = np.array((sign * self.k * np.sin(angle2), 0, self.k * np.cos(angle2)))
         ax_z = np.array((0, 0, 1))
 
-        ttillum = [
+        sources = [
             Sources.PlaneWave(0, b, 0, 0, vec_x),
             Sources.PlaneWave(0, c, 0, 0, vec_mx),
             Sources.PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, 2 * np.pi / 3)),
@@ -309,11 +320,13 @@ class BFPConfiguration:
             Sources.PlaneWave(p, 1j * p, 0, 0, np.array((0, 10 ** -10, self.k))),
         ]
 
-        two_triangles_illumination =  IlluminationPlaneWaves3D.find_ipw_from_pw(ttillum)
-        illumination =  IlluminationPlaneWaves3D.init_from_list(two_triangles_illumination, base_vectors)
+        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
+                                                                      base_vectors,
+                                                                      dimensions=(1, 1, 0), 
+                                                                      Mr = 1, 
+                                                                      store_plane_waves=True)
         illumination.Mt = Mt
         illumination.normalize_spatial_waves()
-
         return illumination
 
     def get_two_oblique_squares_and_one_normal_wave(self, angle1, k_ratio, strength1, strength2,
@@ -338,7 +351,7 @@ class BFPConfiguration:
 
         ax_z = np.array((0, 0, 1))
 
-        ttillum = [
+        sources = [
             Sources.PlaneWave(0, b, 0, 0, vec_x),
             Sources.PlaneWave(0, c, 0, 0, vec_mx),
             Sources.PlaneWave(0, b, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, np.pi / 2)),
@@ -350,9 +363,11 @@ class BFPConfiguration:
             Sources.PlaneWave(1 * p, 1j * p, 0, 0, np.array((0, 10 ** -10, self.k))),
         ]
 
-        two_triangles_illumination = IlluminationPlaneWaves3D.find_ipw_from_pw(ttillum)
-        illumination =  IlluminationPlaneWaves3D.init_from_list(two_triangles_illumination, base_vectors)
+        illumination = IlluminationPlaneWaves3D.init_from_plane_waves(sources,
+                                                                      base_vectors,
+                                                                      dimensions=(1, 1, 0), 
+                                                                      Mr = 1, 
+                                                                      store_plane_waves=True)
         illumination.Mt = Mt
         illumination.normalize_spatial_waves()
-
         return illumination

papers/ssnr_comparison/generate_plots.py
@@ -68,9 +68,9 @@ multiplier = 10 ** 5
 ylim = 10 ** 2
 
 optical_system = System4f3D(alpha=alpha, refractive_index_sample=nobject, refractive_index_medium=nmedium)
-conventional = configurations.get_2_oblique_s_waves_and_s_normal(theta, 1, 1, 3, Mt=1)
-
 optical_system.compute_psf_and_otf((psf_size, N), high_NA=True)
+
+conventional = configurations.get_2_oblique_s_waves_and_s_normal(theta, 1, 1, 3, Mt=1)
 squareL = configurations.get_4_oblique_s_waves_and_s_normal_diagonal(theta, 1, 1, Mt=1)
 squareC = configurations.get_4_circular_oblique_waves_and_circular_normal(theta, 0.58, 1, Mt=1, phase_shift=0)
 hexagonal = configurations.get_6_oblique_s_waves_and_circular_normal(theta, 1, 1, Mt=1)
@@ -340,7 +340,7 @@ class TestArticlePlots(unittest.TestCase):
             ax.set_xlim(0, (2 * NA + 2 * nmedium * np.sin(theta)) / (2 * NA))
             ax.set_ylim(1, 10 ** 2)
             plt.show()
-            fig.savefig(f'{path_to_figures}sim_anisotropy-{phase}')
+            # fig.savefig(f'{path_to_figures}sim_anisotropy-{phase}')
 
 
         # ax1.plot(two_NA_fx[N // 2:], 1 + scaling_factor * ssnr0[:, N // 2, N // 2 + z_shift][N // 2:], label="$k_x$")
@@ -444,7 +444,7 @@ class TestArticlePlots(unittest.TestCase):
         ax2.set_yscale('log')
         ax1.plot(ssnrD0scaled[N//2 + 20, :, N//2])
         ax1.plot(ssnrD1scaled[N//2 + 20, :, N//2])
-        fig.savefig(f'{path_to_figures}sim_anisotropy')
+        # fig.savefig(f'{path_to_figures}sim_anisotropy')
         plt.show()
 
     def test_sim_modalities_comparison(self):

simulations/Recontruction2DSIM.py
@@ -67,7 +67,7 @@ N = 255
 wavelength = 680e-9
 px_scaled = 80e-9
 dx = px_scaled / wavelength
-NA = 1.4 
+NA = 1.1
 nmedium = 1.518
 alpha = np.arcsin(NA / nmedium)
 max_r = dx * N // 2
@@ -86,8 +86,11 @@ print(data.shape)
 
 stack = data.reshape((3, -1, 5, 512, 512))
 stack = stack[:, 3, :, N//2+1:-N//2-1, N//2+1:-N//2-1]
+from windowing import make_mask_cosine_edge2d
+mask = make_mask_cosine_edge2d(stack.shape[2:], 10)
+stack = stack * mask[np.newaxis, np.newaxis, ...]
 print(stack.shape)
-plt.imshow(stack[0, 0, ...].T)
+plt.imshow(stack[0, 0, ...].T, cmap='gray', origin='lower')
 plt.show()
 plt.imshow(np.log1p(np.abs(wrapped_fftn(stack[0, 0, ...]))).T, cmap='gray', origin='lower')
 plt.show()
@@ -146,28 +149,35 @@ recontructor_spatial = ReconstructorSpatialDomain2D(
 recontructor_finite = ReconstructorSpatialDomain2D(
     illumination=illumination,
     optical_system=optical_system,
-    kernel=kernels.sinc_kernel(5)[..., 0])
+    kernel=kernels.psf_kernel2d(7, (dx, dx))
+)
 
 reconstructed_fourier = reconstructor_fourier.reconstruct(stack)
-recontructed_spatial = recontructor_spatial.reconstruct(stack)
-recontructed_finite = recontructor_finite.reconstruct(stack)
+reconstructed_spatial = recontructor_spatial.reconstruct(stack)
+reconstructed_finite = recontructor_finite.reconstruct(stack)
 widefield  = reconstructor_fourier.get_widefield(stack)
+
+scaling_spatial = np.amax(reconstructed_fourier) / np.amax(reconstructed_spatial)
+scaling_finite = np.amax(reconstructed_fourier) / np.amax(reconstructed_finite)
+
+reconstructed_spatial *= scaling_spatial
+reconstructed_finite *= scaling_finite
 
 fig, ax = plt.subplots(2, 4, figsize=(15, 5))
 fig.suptitle('Reconstructed images')
 ax[0, 0].imshow(np.log1p(np.abs(wrapped_fftn(reconstructed_fourier))).T, cmap='gray', origin='lower')
 ax[0, 0].set_title('Fourier')
-ax[0, 1].imshow(np.log1p(np.abs(wrapped_fftn(recontructed_spatial))).T, cmap='gray', origin='lower')
+ax[0, 1].imshow(np.log1p(np.abs(wrapped_fftn(reconstructed_spatial))).T, cmap='gray', origin='lower')
 ax[0, 1].set_title('Spatial')
-ax[0, 2].imshow(np.log1p(np.abs(wrapped_fftn(recontructed_finite))).T, cmap='gray', origin='lower')
+ax[0, 2].imshow(np.log1p(np.abs(wrapped_fftn(reconstructed_finite))).T, cmap='gray', origin='lower')
 ax[0, 2].set_title('Finite')
 ax[0, 3].imshow(np.log1p(np.abs(wrapped_fftn(widefield))).T, cmap='gray', origin='lower')
 ax[0, 3].set_title('Widefield')
 ax[1, 0].imshow(reconstructed_fourier.T, cmap='gray', origin='lower')
 ax[1, 0].set_title('Fourier')
-ax[1, 1].imshow(recontructed_spatial.T, cmap='gray', origin='lower')
+ax[1, 1].imshow(reconstructed_spatial.T, cmap='gray', origin='lower')
 ax[1, 1].set_title('Spatial')
-ax[1, 2].imshow(recontructed_finite.T, cmap='gray', origin='lower')
+ax[1, 2].imshow(reconstructed_finite.T, cmap='gray', origin='lower')
 ax[1, 2].set_title('Finite')
 ax[1, 3].imshow(widefield.T, cmap='gray', origin='lower')
 ax[1, 3].set_title('Widefield')
@@ -190,80 +200,77 @@ ssnr_finite = SSNRSIM2D(
     illumination=illumination,
     optical_system=optical_system,
     readout_noise_variance=1,
-    kernel=kernels.sinc_kernel(5)[..., 0]
+    kernel=kernels.psf_kernel2d(7, (dx, dx))
 )
 
 fig, ax = plt.subplots(2, 3, figsize=(15, 5))
 fig.suptitle('SSNR images')
-ax[0, 0].imshow(np.log1p(np.abs(ssnr_fourier.ssnri)).T, cmap='gray', origin='lower')
+ax[0, 0].imshow(np.log1p(10 ** 8 * np.abs(ssnr_fourier.ssnri)).T, cmap='gray', origin='lower')
 ax[0, 0].set_title('Fourier')
-ax[0, 1].imshow(np.log1p(np.abs(ssnr_spatial.ssnri)).T, cmap='gray', origin='lower')
+ax[0, 1].imshow(np.log1p(10 ** 8 * (np.abs(ssnr_spatial.ssnri))).T, cmap='gray', origin='lower')
 ax[0, 1].set_title('Spatial')
-ax[0, 2].imshow(np.log1p(np.abs(ssnr_finite.ssnri)).T, cmap='gray', origin='lower')
+ax[0, 2].imshow(np.log1p(10 ** 8 * np.abs(ssnr_finite.ssnri)).T, cmap='gray', origin='lower')
 ax[0, 2].set_title('Finite')
 ax[1, 0].plot((ssnr_spatial.ssnri/ssnr_fourier.ssnri)[N//2, N//2:])
 ax[1, 0].plot((ssnr_finite.ssnri/ssnr_fourier.ssnri)[N//2, N//2:])
 ax[1, 1].imshow(ssnr_spatial.ssnri/ssnr_fourier.ssnri, origin='lower')
 ax[1, 2].imshow(ssnr_finite.ssnri/ssnr_fourier.ssnri, origin='lower')
-
+ax[1, 0].plot((ssnr_spatial.ssnri/ssnr_fourier.ssnri)[N//2, N//2:])
+ax[1, 0].plot((ssnr_finite.ssnri/ssnr_fourier.ssnri)[N//2, N//2:])
+ax[1, 1].imshow(ssnr_spatial.ssnri/ssnr_fourier.ssnri, origin='lower')
+ax[1, 2].imshow(ssnr_finite.ssnri/ssnr_fourier.ssnri, origin='lower')
 plt.show()
 
-# filtered_fourier, w_fourier = filter_true_wiener(
-#     wrapped_fftn(reconstructed_fourier), 
-#     ssnr_fourier.otf_sim,
-#     ssnr_fourier,
-# )
+filtered_fourier, w_fourier, ssnr_fourier_measured = filter_true_wiener(
+    wrapped_fftn(reconstructed_fourier), 
+    ssnr_fourier,
+)
 
-# filtered_spatial, w_spatial = filter_true_wiener(
-#     wrapped_fftn(recontructed_spatial), 
-#     ssnr_spatial.otf_sim,
-#     ssnr_spatial,
-# )
+filtered_spatial, w_spatial, ssnr_spatial_measured = filter_true_wiener(
+    wrapped_fftn(reconstructed_spatial), 
+    ssnr_spatial,
+)
 
-# filtered_finite, w_finite = filter_true_wiener(
-#     wrapped_fftn(recontructed_finite), 
-#     ssnr_finite.otf_sim,
-#     ssnr_finite,
-# )
+filtered_finite, w_finite, ssnr_finite_measured = filter_true_wiener(
+    wrapped_fftn(reconstructed_finite), 
+    ssnr_finite,
+)
 
 # filtered_fourier, w_fourier = filter_flat_noise(
 #     wrapped_fftn(reconstructed_fourier), 
-#     ssnr_fourier.otf_sim,
 #     ssnr_fourier,
 # )
 
 # filtered_spatial, w_spatial = filter_flat_noise(
 #     wrapped_fftn(recontructed_spatial), 
-#     ssnr_spatial.otf_sim,
 #     ssnr_spatial,
 # )
 
 # filtered_finite, w_finite = filter_flat_noise(
 #     wrapped_fftn(recontructed_finite), 
-#     ssnr_finite.otf_sim,
 #     ssnr_finite,
 # )
 
-filtered_fourier, w_fourier = filter_constant(
-    wrapped_fftn(reconstructed_fourier), 
-    ssnr_fourier.otf_sim,
-    1e-4,
-)
+# filtered_fourier, w_fourier = filter_constant(
+#     wrapped_fftn(reconstructed_fourier), 
+#     ssnr_fourier.dj,
+#     1e-2,
+# )
 
-filtered_spatial, w_spatial = filter_constant(
-    wrapped_fftn(recontructed_spatial), 
-    ssnr_spatial.otf_sim,
-    1e-3,
-)
+# filtered_spatial, w_spatial = filter_constant(
+#     wrapped_fftn(recontructed_spatial), 
+#     ssnr_spatial.dj,
+#     1e-2,
+# )
 
-filtered_finite, w_finite = filter_constant(
-    wrapped_fftn(recontructed_finite), 
-    ssnr_finite.otf_sim,
-    1e-4,
-)
+# filtered_finite, w_finite = filter_constant(
+#     wrapped_fftn(recontructed_finite), 
+#     ssnr_finite.dj,
+#     1e-2,
+# )
     
     
-fig, ax = plt.subplots(2, 4, figsize=(15, 5))
+fig, ax = plt.subplots(3, 4, figsize=(15, 5))
 fig.suptitle('Filtered images')
 ax[0, 0].imshow(np.log1p(np.abs((filtered_fourier))).T, cmap='gray', origin='lower')
 ax[0, 0].set_title('Fourier')
@@ -275,12 +282,21 @@ ax[0, 3].imshow(np.log1p(np.abs(wrapped_fftn(widefield))).T, cmap='gray', origin
 ax[0, 3].set_title('Widefield')
 ax[1, 0].imshow(np.abs(wrapped_ifftn(filtered_fourier)).T, cmap='gray', origin='lower')
 ax[1, 0].set_title('Fourier')
-ax[1, 1].imshow(np.abs(wrapped_ifftn(filtered_fourier)).T, cmap='gray', origin='lower')
+ax[1, 1].imshow(np.abs(wrapped_ifftn(filtered_spatial)).T, cmap='gray', origin='lower')
 ax[1, 1].set_title('Spatial')
-ax[1, 2].imshow(np.abs(wrapped_ifftn(filtered_fourier)).T, cmap='gray', origin='lower')
+ax[1, 2].imshow(np.abs(wrapped_ifftn(filtered_finite)).T, cmap='gray', origin='lower')
 ax[1, 2].set_title('Finite')
 ax[1, 3].imshow(widefield.T, cmap='gray', origin='lower')
 ax[1, 3].set_title('Widefield')
+ax[2, 0].imshow(np.log1p(ssnr_fourier_measured), cmap='gray', origin='lower')
+ax[2, 0].set_title('Fourier')
+ax[2, 1].imshow(np.log1p(ssnr_spatial_measured), cmap='gray', origin='lower')
+ax[2, 1].set_title('Spatial')
+ax[2, 2].imshow(np.log1p(ssnr_finite_measured), cmap='gray', origin='lower')
+ax[2, 2].set_title('Finite')
+ax[2, 3].plot((ssnr_spatial_measured/ssnr_fourier_measured)[N//2, N//2:], label='spatial/fourier')
+ax[2, 3].plot((ssnr_finite_measured /ssnr_fourier_measured )[N//2, N//2:], label='finite/fourier')
+ax[2, 3].legend()
 plt.show()
 
 apodization = AutoconvolutuionApodizationSIM2D(
@@ -293,9 +309,9 @@ plt.imshow(np.abs(ideal_otf), cmap='gray')
 plt.title('Ideal OTF')
 plt.show()
 
-apodized_fourier = wrapped_ifftn(ideal_otf * filtered_fourier)
-apodized_spatial = wrapped_ifftn(ideal_otf * filtered_spatial)
-apodized_finite = wrapped_ifftn(ideal_otf * filtered_finite)
+apodized_fourier = wrapped_ifftn(ideal_otf * 10**8 * filtered_fourier)
+apodized_spatial =  wrapped_ifftn(ideal_otf *  10**8 * filtered_spatial)
+apodized_finite = wrapped_ifftn(ideal_otf *  10**8 * filtered_finite)
 
 fig, ax = plt.subplots(1, 4, figsize=(15, 5))
 fig.suptitle('Apodized images')

unittests/test_SNR_calculator.py
@@ -508,8 +508,7 @@ class TestSSNRSIM(unittest.TestCase):
         ylim = 10**2
 
         optical_system = System4f3D(alpha=alpha, refractive_index_sample=nobject, refractive_index_medium=nmedium)
-        optical_system.compute_psf_and_otf((psf_size, N),
-                                           apodization_function="Sine")
+        optical_system.compute_psf_and_otf((psf_size, N))
 
         illumination_s_polarized = configurations.get_4_oblique_s_waves_and_circular_normal(theta, 1, 1, Mt=32)
         illumination_circular = configurations.get_4_circular_oblique_waves_and_circular_normal(theta, 0.55, 1, Mt=64)
@@ -522,9 +521,9 @@ class TestSSNRSIM(unittest.TestCase):
         ssnr_widefield = noise_estimator_widefield.ssnri
         ssnr_widefield_ra = noise_estimator_widefield.ring_average_ssnri()
 
-        noise_estimator_confocal = SSNRConfocal(optical_system)
-        ssnr_confocal = noise_estimator_confocal.ssnr
-        ssnr_confocal_ra = noise_estimator_confocal.ring_average_ssnri()
+        # noise_estimator_confocal = SSNR(optical_system)
+        # ssnr_confocal = noise_estimator_confocal.ssnr
+        # ssnr_confocal_ra = noise_estimator_confocal.ring_average_ssnri()
 
         noise_estimator = SSNRSIM3D(illumination_s_polarized, optical_system)
         # noise_estimator_true = SSNRSIM3D3dShifts(illumination_s_polarized, optical_system)
@@ -694,7 +693,7 @@ class TestSSNRSIM(unittest.TestCase):
             ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_seven_waves[:, int(val), int(N / 2)][fx >= 0],    label="7 waves"   )
             ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_3waves[:, int(val),  int(N / 2)][fx >= 0],      label="3 waves"    )
             ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_widefield[:, int(val), int(N / 2)][fx >= 0],   label="Widefield"  )
-            ax2.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_confocal[:, int(val), int(N / 2)][fx >= 0], label="Confocal")
+            # ax2.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_confocal[:, int(val), int(N / 2)][fx >= 0], label="Confocal")
             ax2.legend()
             ax2.set_aspect(1. / ax2.get_data_ratio())
 
@@ -993,30 +992,30 @@ class TestSSNRSIM(unittest.TestCase):
             # fig.savefig(f'{path_to_figures}'
             #          + illumination_list[illumination][1] + '_ssnr.png')
 
-            # def update1(val):
-            #     ax1.set_title("ssnr, fy = {:.2f}, ".format(fy[int(val)]) + "$\\frac{2NA}{\\lambda}$")
-            #     ax1.set_xlabel("fz, $\lambda^{-1}$")
-            #     ax1.set_ylabel("fx, $\lambda^{-1}$")
-            #     Z = (ssnr_scaled[:, int(val), :])
-            #     mp1.set_data(Z)
-            #     mp1.set_clim(vmin=Z.min(), vmax=Z.max())
-            #     ax1.set_aspect(1. / ax1.get_data_ratio())
-            #     plt.draw()
-            #
-            #     ax2.set_title("ssnr, fz = {:.2f}, ".format(fz[int(val)]) + "$\\frac{2NA}{\\lambda}$")
-            #     ax2.set_xlabel("fz, $\lambda^{-1}$")
-            #     ax2.set_ylabel("fx, $\lambda^{-1}$")
-            #     Z = (ssnr_scaled[:, :, int(val)])
-            #     mp2.set_data(Z)
-            #     mp2.set_clim(vmin=Z.min(), vmax=Z.max())
-            #     ax2.set_aspect(1. / ax2.get_data_ratio())
-            #     plt.draw()
-            #
-            # slider_loc = plt.axes((0.2, 0.0, 0.3, 0.03))  # slider location and size
-            # slider_ssnr = Slider(slider_loc, 'fz', 0, N - 1)  # slider properties
-            # slider_ssnr.on_changed(update1)
+            def update1(val):
+                ax1.set_title("ssnr, fy = {:.2f}, ".format(fy[int(val)]) + "$\\frac{2NA}{\\lambda}$")
+                ax1.set_xlabel("fz, $\lambda^{-1}$")
+                ax1.set_ylabel("fx, $\lambda^{-1}$")
+                Z = (ssnr_scaled[:, int(val), :])
+                mp1.set_data(Z)
+                mp1.set_clim(vmin=Z.min(), vmax=Z.max())
+                ax1.set_aspect(1. / ax1.get_data_ratio())
+                plt.draw()
+            
+                ax2.set_title("ssnr, fz = {:.2f}, ".format(fz[int(val)]) + "$\\frac{2NA}{\\lambda}$")
+                ax2.set_xlabel("fz, $\lambda^{-1}$")
+                ax2.set_ylabel("fx, $\lambda^{-1}$")
+                Z = (ssnr_scaled[:, :, int(val)])
+                mp2.set_data(Z)
+                mp2.set_clim(vmin=Z.min(), vmax=Z.max())
+                ax2.set_aspect(1. / ax2.get_data_ratio())
+                plt.draw()
+            
+            slider_loc = plt.axes((0.2, 0.0, 0.3, 0.03))  # slider location and size
+            slider_ssnr = Slider(slider_loc, 'fz', 0, N - 1)  # slider properties
+            slider_ssnr.on_changed(update1)
 
-            # plt.show()
+            plt.show()
 
     def test_alternative_visualisations(self):
         theta = np.pi / 4
@@ -1196,174 +1195,6 @@ class TestSSNRSIM(unittest.TestCase):
         # fig.savefig(f'{path_to_figures}fz={:.2f}_compare_ssnr_conf_version.png'.format(two_NA_fz[arg]))
         plt.show()
 
-    def testssnrFromNumericalspatialWaves(self):
-        max_r = 10
-        max_z = 40
-        N = 100
-        psf_size = 2 * np.array((max_r, max_r, max_z))
-        dx = 2 * max_r / N
-        dy = 2 * max_r / N
-        dz = 2 * max_z / N
-        dV = dx * dy * dz
-        x = np.arange(-max_r, max_r, dx)
-        y = np.copy(x)
-        z = np.arange(-max_z, max_z, dz)
-
-        fx = np.linspace(-1 / (2 * dx), 1 / (2 * dx) - 1 / (2 * max_r), N)
-        fy = np.linspace(-1 / (2 * dy), 1 / (2 * dy) - 1 / (2 * max_r), N)
-        fz = np.linspace(-1 / (2 * dz), 1 / (2 * dz) - 1 / (2 * max_z), N)
-
-        arg = N // 2  # - 24
-
-        NA = np.sin(np.pi/3)
-        two_NA_fx = fx / (2 * NA)
-        two_NA_fy = fy / (2 * NA)
-        two_NA_fz = fz / (2 * NA)
-
-        optical_system = System4f3D(alpha = np.pi/3)
-        optical_system.compute_psf_and_otf((psf_size, N),
-                                           apodization_function="Sine")
-
-        k = 2 * np.pi
-        theta = np.pi/3
-        vec_x = np.array((k * np.sin(np.pi/3), 0, k * np.cos(np.pi/3)))
-        vec_mx = np.array((-k * np.sin(np.arcsin(5/7 * np.sin(np.pi/3))), 0, k * np.cos(np.arcsin(5/7 * np.sin(np.pi/3)))))
-        ax_z = np.array((0, 0, 1))
-
-        sources = [
-            Sources.PlaneWave(0, 1, 0, 0, vec_x),
-            Sources.PlaneWave(0, 1, 0, 0, vec_mx),
-            Sources.PlaneWave(0, 1, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, 2 * np.pi/3)),
-            Sources.PlaneWave(0, 1, 0, 0, VectorOperations.rotate_vector3d(vec_mx, ax_z, 2 * np.pi/3)),
-            Sources.PlaneWave(0, 1, 0, 0, VectorOperations.rotate_vector3d(vec_x, ax_z, 4 * np.pi/3)),
-            Sources.PlaneWave(0, 1, 0, 0, VectorOperations.rotate_vector3d(vec_mx, ax_z, 4 * np.pi/3)),
-
-            Sources.PlaneWave(1, 1j, 0, 0, np.array((0, 10**-10, 2 * np.pi))),
-        ]
-        size = (2 * max_r, 2 * max_r, 2 * max_z)
-        box = Box.Box(sources, size, N)
-        box.compute_intensity_and_spatial_waves_numerically()
-        iwaves = box.get_approximated_intensity_sources()
-        illumination_2z =  IlluminationPlaneWaves3D.init_from_list(iwaves, (k * np.sin(theta) / 14, k * np.sin(theta) * 3**0.5/ 14, k / 14), Mr = 1)
-        illumination_2z.normalize_spatial_waves()
-        noise_estimator = SSNRSIM3D(illumination_2z, optical_system)
-        ssnr_2z = np.abs(noise_estimator.ssnri)
-        ssnr_2z_ra = noise_estimator.ring_average_ssnri()
-        volume_2z = noise_estimator.compute_ssnri_volume(ssnr_2z, dV)
-        b = 1/2**0.5
-        sources = [
-            Sources.PlaneWave(0, b, 0, 0, np.array((0, k * NA,k * np.cos(np.pi/3)))),
-            Sources.PlaneWave(0, -b, 0, 0, np.array((0, -k * NA, k * np.cos(np.pi/3)))),
-            Sources.PlaneWave(0, 1, 0, 0, np.array((0, 10 ** -10, k))),
-        ]
-        box = Box.Box(sources, size, N)
-        box.compute_intensity_and_spatial_waves_numerically()
-        iwaves = box.get_approximated_intensity_sources()
-        il =  IlluminationPlaneWaves3D.index_harmonics(iwaves, (10**10 , k * np.sin(theta), k * (1 - np.cos(theta))))
-        illumination_3w = IlluminationPlaneWaves3D(il, Mr=5)
-        illumination_3w.Mt = 1
-        illumination_3w.normalize_spatial_waves()
-        noise_estimator = SSNRSIM3D(illumination_3w, optical_system)
-        ssnr_3w = np.abs(noise_estimator.ssnri)
-        ssnr_3w_ra = noise_estimator.ring_average_ssnri()
-        volume_3w = noise_estimator.compute_ssnri_volume(ssnr_3w, dV)
-
-        widefield = IlluminationPlaneWaves3D({
-        (0, 0, 0) : Sources.IntensityHarmonic3D(1, 0, np.array((0, 0, 0)))}, Mr=1)
-        widefield.Mt = 1
-        noise_estimator.illumination = widefield
-        ssnr_widefield = np.abs(noise_estimator.ssnri)
-        ssnr_widefield_ra= noise_estimator.ring_average_ssnri()
-        volume_w = noise_estimator.compute_ssnri_volume(ssnr_widefield, dV)
-        print("volume 3 waves = ", volume_3w)
-        print("volume two triangles = ", volume_2z)
-        print("volume widefield = ", volume_w)
-        Fx, Fy = np.meshgrid(fx, fy)
-        fig = plt.figure(figsize=(12, 6), constrained_layout=True)
-        plt.subplots_adjust(left=0.1,
-                            bottom=0.1,
-                            right=0.9,
-                            top=0.9,
-                            wspace=0.4,
-                            hspace=0.4)
-        ax1 = fig.add_subplot(121)
-        ax2 = fig.add_subplot(122)
-
-        ax1.set_title("Ring averaged", fontsize=20, pad=15)
-        ax1.set_xlabel(r"$f_r$", fontsize=20)
-        ax1.set_ylabel(r"$ssnr_{ra}$", fontsize=20)
-        ax1.set_yscale("log")
-        ax1.set_ylim(1, 3 * 10 ** 2)
-        ax1.set_xlim(0, fx[-1] / (2 * NA))
-        ax1.grid(which='major')
-        ax1.grid(which='minor', linestyle='--')
-        ax1.tick_params(labelsize=15)
-
-        ax2.set_title("Slice $f_y$ = 0", fontsize=20)
-        ax2.set_xlabel(r"$f_x$", fontsize=20)
-        ax2.set_yscale("log")
-        ax2.set_ylim(1, 3 * 10 ** 2)
-        ax2.set_xlim(0, fx[-1] / (2 * NA))
-        ax2.grid(which='major')
-        ax2.grid(which='minor', linestyle='--')
-        ax2.tick_params('y', labelleft=False)
-        ax2.tick_params(labelsize=15)
-
-        multiplier = 10 ** 8
-        ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_2z_ra[:, arg], label="2 triangles")
-        ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_3w_ra[:, arg], label="3 waves")
-        ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_widefield_ra[:, arg], label="widefield")
-
-        ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_2z[:, int(N / 2), arg][fx >= 0], label="5 waves, 2 z angles")
-        ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_3w[:, int(N / 2), arg][fx >= 0], label="3 waves")
-        ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_widefield[:, int(N / 2), arg][fx >= 0], label="widefield")
-        ax1.set_aspect(1. / ax1.get_data_ratio())
-        ax2.set_aspect(1. / ax2.get_data_ratio())
-
-        def update1(val):
-            ax1.clear()
-            ax1.set_title("Ring averaged", fontsize=30)
-            ax1.set_xlabel(r"$f_r$",  fontsize=25)
-            ax1.set_ylabel(r"$ssnr$", fontsize=25)
-            ax1.set_yscale("log")
-            ax1.set_ylim(1, 3 * 10 ** 2)
-            ax1.set_xlim(0, fx[-1] / (2 * NA))
-            ax1.grid()
-            ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_2z_ra[:, int(val)], label=" 5 waves, 2 z angles")
-            ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_3w_ra[:, int(val)], label=" 3 waves")
-            ax1.plot(two_NA_fx[fx >= 0], 1 + multiplier * ssnr_widefield_ra[:, int(val)], label="widefield")
-
-            ax1.legend()
-            ax1.set_aspect(1. / ax1.get_data_ratio())
-
-            ax2.clear()
-            ax2.set_title("Slice $f_y$ = 0")
-            ax2.set_xlabel(r"$f_x$")
-            # ax2.set_ylabel(r"ssnr")
-
-            ax2.set_yscale("log")
-            ax2.set_ylim(1, 3 * 10 ** 2)
-            ax2.set_xlim(0, fx[-1] / (2 * NA))
-            ax2.grid()
-
-            ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_2z[:, int(N / 2), int(val)][fx >= 0], label="5 waves, 2z angles")
-            ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_3w[:, int(N / 2), int(val)][fx >= 0], label="3 waves")
-            ax2.plot(two_NA_fy[fy >= 0], 1 + multiplier * ssnr_widefield[:, int(N / 2), int(val)][fx >= 0], label="widefield")
-
-            ax2.legend()
-            ax2.set_aspect(1. / ax2.get_data_ratio())
-
-        slider_loc = plt.axes((0.2, 0.0, 0.3, 0.03))  # slider location and size
-        slider_ssnr = Slider(slider_loc, 'fz', 0, 100)  # slider properties
-        slider_ssnr.on_changed(update1)
-
-        ax1.legend()
-        ax2.legend()
-        # fig.savefig(
-        #     f'{path_to_figures}fz={:.2f}_compare_ssnr_conf_version.png'.format(
-        #         fz[arg]))
-        plt.show()
-
     def testFourWavesVSFiveWaves(self):
         max_r = 10
         max_z = 20
@@ -1528,8 +1359,7 @@ class TestApproximations(unittest.TestCase):
         two_NA_fz = fz / (1 - np.cos(alpha))
 
         optical_system = System4f3D(alpha=alpha)
-        optical_system.compute_psf_and_otf((psf_size, N),
-                                           apodization_function="Sine")
+        optical_system.compute_psf_and_otf((psf_size, N), high_NA=False)
 
         illumination_s_polarized = configurations.get_4_oblique_s_waves_and_circular_normal(theta, 1, 1, Mt=32)
         illumination_circular = configurations.get_4_circular_oblique_waves_and_circular_normal(theta, 0.55, 1, Mt=64)