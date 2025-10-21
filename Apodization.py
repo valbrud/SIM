Fixed numerical bugs

Apodization.py
@@ -161,7 +161,7 @@ class AutoconvolutuionApodizationSIM2D(AutoconvolutionApodizationSIM):
                 ideal_pupil_function[phase_shifted >  0.1 * np.amax(self.pupil_function)] = 1
 
         self._ideal_ctf = ideal_pupil_function
-        self._ideal_otf = np.flip(scipy.signal.convolve(self._ideal_ctf, np.flip(self._ideal_ctf).conjugate(), mode='same'))
+        self._ideal_otf = scipy.signal.convolve(self._ideal_ctf, np.flip(self._ideal_ctf).conjugate(), mode='same')
         self._ideal_otf /= np.amax(self._ideal_otf)
         self._ideal_psf = hpc_utils.wrapped_ifftn(self._ideal_otf).real
         self._ideal_psf /= np.sum(self._ideal_psf)

OpticalSystems.py
@@ -320,6 +320,8 @@ class OpticalSystem2D(OpticalSystem):
         fx = np.linspace(-Nx / (2 * Lx), Nx / (2 * Lx), Nx)
         fy = np.linspace(-Ny / (2 * Ly), Ny / (2 * Ly), Ny)
         self._otf_frequencies = (fx, fy)
+        self._x_grid = None
+        self._q_grid = None
 
 
     def interpolate_otf(self, k_shift: ndarray[3, np.float64]) -> ndarray[tuple[int, int, int], np.float64]:
@@ -472,7 +474,7 @@ class System4f2DCoherent(OpticalSystem2D):
         
         self._normalize_psf__and_otf()
 
-        self._prepare_interpolator()
+        # self._prepare_interpolator()
         return self.psf, self.otf
 
 
@@ -527,7 +529,7 @@ class System4f3DCoherent(OpticalSystem3D):
         
         self._normalize_psf__and_otf()
 
-        self._prepare_interpolator()
+        # self._prepare_interpolator()
         return self.psf, self.otf
 
 
@@ -581,14 +583,14 @@ class System4f2D(System4f2DCoherent):
             )
 
             if self.computed_size:
-                utils.expand_kernel(psf, (self.psf_coordinates[0], self.psf_coordinates[1]))
+                psf = utils.expand_kernel(psf, (self.psf_coordinates[0].size, self.psf_coordinates[1].size))
 
         self.psf = psf.real 
         self.otf = hpc_utils.wrapped_fftn(self.psf)
         
         self._normalize_psf__and_otf()
 
-        self._prepare_interpolator()
+        # self._prepare_interpolator()
         return self.psf, self.otf
 
 
@@ -646,7 +648,7 @@ class System4f3D(System4f3DCoherent):
             self.otf /= np.amax(self.otf)
             self.psf = psf / np.sum(psf)
 
-        self._prepare_interpolator()
+        # self._prepare_interpolator()
         return self.psf, self.otf
 
 

SSNRCalculator.py
@@ -13,7 +13,7 @@ import numpy as np
 from Illumination import PlaneWavesSIM, IlluminationPlaneWaves2D, IlluminationPlaneWaves3D
 import OpticalSystems
 import hpc_utils
-from utils import average_rings2d
+from utils import average_rings2d, expand_ring_averages2d
 import VectorOperations
 import matplotlib.pyplot as plt
 from abc import abstractmethod
@@ -376,34 +376,6 @@ class SSNRSIM(SSNRBase):
         return ((self.dj * np.abs(object_ft)) ** 2 /
                 (np.amax(np.abs(object_ft)) * self.vj + self.optical_system.otf.size * self.readout_noise_variance * self.dj))
 
-    def ring_average_ssnri_approximated(self, number_of_samples=None, mask=None):
-        """
-        Compute ring avergaged ssnri as <Dj^2> / <Vj> instead of <Dj^2 / Vj>
-        """
-        q_axes = self.optical_system.otf_frequencies
-        dj = np.copy(self.dj)
-        vj = np.copy(self.vj)
-        
-        if len(q_axes) == 2:
-            if mask is not None:
-                dja = utils.average_mask(dj, mask, shape='reduced')
-                vja = utils.average_mask(vj, mask, shape='reduced')
-                return np.nan_to_num(dja**2 / vja)
-            else:
-                return average_rings2d(dj**2, q_axes, number_of_samples=number_of_samples) / average_rings2d(vj, q_axes, number_of_samples=number_of_samples)
-        
-        elif len(q_axes) != 3:
-            raise AttributeError("PSF dimension is not equal to 2 or 3!")
-
-        averaged_slices = []
-        for i in range(dj.shape[2]):
-            if mask is not None:
-                dja = utils.average_mask(dj[i], mask, shape='reduced')
-                vja = utils.average_mask(vj[i], mask, shape='reduced')
-                averaged_slices.append(np.nan_to_num(dja**2 / vja))
-            else:
-                averaged_slices.append(average_rings2d(dj[i]**2, q_axes, number_of_samples=number_of_samples) / average_rings2d(vj[i], q_axes, number_of_samples=number_of_samples))
-        return np.array(averaged_slices).T
 
     def compute_analytic_ssnri_volume(self, factor=10, volume_element=1):
         g2 = np.sum(self.optical_system.otf * self.optical_system.otf.conjugate()).real
@@ -481,6 +453,51 @@ class SSNRSIM2D(SSNRSIM):
         
         super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory, illumination_reconstruction=illumination_reconstruction)
 
+    def ssnri_like_sectorial_average(self,  mask=None, degree_of_symmetry=1, theta0=0.):
+        """
+        Compute avergaged ssnri as <Dj^2> / <Vj> in a sector instead of <Dj^2 / Vj>
+        """
+        q_axes = self.optical_system.otf_frequencies
+        dj = np.copy(self.dj)
+        vj = np.copy(self.vj)
+        
+        if len(q_axes) != 2:
+            raise AttributeError("PSF dimensionality is not equal to 2!")
+        
+        if mask is not None:
+            dja = utils.average_mask(dj, mask, shape='reduced')
+            vja = utils.average_mask(vj, mask, shape='reduced')
+            return np.nan_to_num(dja**2 / vja)
+        else:
+            return average_rings2d(dj**2, q_axes, degree_of_symmetry, theta0) / average_rings2d(vj, q_axes, degree_of_symmetry, theta0)
+        
+
+    def ssnr_like_sectorial_average_from_image(self, image_ft, degree_of_symmetry=1, theta0=0.):
+        if image_ft.ndim != self.dimensionality:
+            raise ValueError("The image and SSNR calculator dimensions do not match.")
+
+        center = np.array(image_ft.shape)//2
+        # print(image_ft[*center])
+        # print(self.dj[*center])
+        f0 = image_ft[*center] / self.dj[*center]
+        bj2 = np.abs(image_ft) ** 2
+
+        obj2sa = average_rings2d(bj2, self.optical_system.otf_frequencies, degree_of_symmetry, theta0)    
+        djsa = average_rings2d(np.copy(self.dj), self.optical_system.otf_frequencies, degree_of_symmetry, theta0)
+        vjsa = average_rings2d(np.copy(self.vj), self.optical_system.otf_frequencies, degree_of_symmetry, theta0)
+
+        numeric_noise = 10**-12
+        # print('total_counts', f0)
+        # noise_power_ra = vjra * f0 + image_ft.size * self.readout_noise_variance**2 * djra
+        noise_power = vjsa * f0 + image_ft.size * self.readout_noise_variance**2 * djsa
+        ssnr_sa = ((obj2sa - noise_power ) /
+                    noise_power).real
+        ssnr_sa = np.nan_to_num(ssnr_sa)
+        ssnr_sa = np.where(djsa > numeric_noise, ssnr_sa, 0)
+        
+        return ssnr_sa
+
+
     def plot_effective_kernel_and_otf(self):
         Nx, Ny = self.optical_system.otf.shape
         fig, ax = plt.subplots()
@@ -522,3 +539,24 @@ class SSNRSIM3D(SSNRSIM):
         if not isinstance(illumination_reconstruction, IlluminationPlaneWaves3D) and not illumination_reconstruction is None:
             raise AttributeError("Illumination reconstruction data is not of the valid dimension!")
         super().__init__(illumination, optical_system, kernel=kernel, readout_noise_variance=readout_noise_variance, save_memory=save_memory, illumination_reconstruction=illumination_reconstruction)
+
+    def ssnri_like_sectorial_average(self, number_of_samples=None, mask=None):
+        """
+        Compute ring avergaged ssnri as <Dj^2> / <Vj> instead of <Dj^2 / Vj>
+        """
+        q_axes = self.optical_system.otf_frequencies
+        dj = np.copy(self.dj)
+        vj = np.copy(self.vj)
+        
+        if len(q_axes) != 3:
+            raise AttributeError("PSF dimension is not equal to 3!")
+
+        averaged_slices = []
+        for i in range(dj.shape[2]):
+            if mask is not None:
+                dja = utils.average_mask(dj[i], mask, shape='reduced')
+                vja = utils.average_mask(vj[i], mask, shape='reduced')
+                averaged_slices.append(np.nan_to_num(dja**2 / vja))
+            else:
+                averaged_slices.append(average_rings2d(dj[i]**2, q_axes, number_of_samples=number_of_samples) / average_rings2d(vj[i], q_axes, number_of_samples=number_of_samples))
+        return np.array(averaged_slices).T

unittests/test_apodization.py
@@ -76,13 +76,13 @@ class TestAutoconvolutionSIM2D(unittest.TestCase):
         plt.show()
 
     def test_apodization_sim(self):
-        conventional = self.configurations.get_2_oblique_s_waves_and_s_normal(self.theta, 1, 0, 3, Mt=1)
+        conventional = self.configurations.get_2_oblique_s_waves_and_s_normal(self.theta, 1, 0, 3, Mt=1, angles=(0 + np.pi/4, np.pi/3 + np.pi/4, 2*np.pi/3 + np.pi/4))
         conventional = IlluminationPlaneWaves2D.init_from_3D(conventional)    
         apodization = AutoconvolutuionApodizationSIM2D(self.optical_system, conventional)
         fig, axes = plt.subplots(1, 3, figsize=(10, 5))
         axes[0].imshow(apodization.ideal_ctf, cmap='gray')
         axes[0].set_title('Ideal CTF')
-        axes[1].imshow(apodization.ideal_otf, cmap='gray')
+        axes[1].imshow(np.log1p(10**8 * apodization.ideal_otf), cmap='gray')
         axes[1].set_title('Ideal OTF')
         axes[2].imshow(apodization.ideal_psf, cmap='gray')
         plt.show()

unittests/test_frc.py
@@ -19,7 +19,7 @@ sys.path.append(project_root)
 # Import required modules
 from ShapesGenerator import generate_random_lines
 from OpticalSystems import System4f2D
-from ResolutionMeasures import frc, frc_one_image
+from resolution_measures import frc, frc_one_image
 import scipy.signal
 
 

utils.py
@@ -160,7 +160,7 @@ def average_mask(array: np.ndarray[np.float64], mask: np.ndarray[np.int32], shap
 #     # theta = np.arange(0, 2 * np.pi, 2 * np.pi / r.size)
 
 
-def average_rings2d(array: np.ndarray, axes: tuple[np.ndarray] = None, num_angles=360, number_of_samples: int = None):
+def average_rings2d(array: np.ndarray, axes: tuple[np.ndarray] = None, degree_of_symmetry=1, theta0 = 0.,  num_angles=360, number_of_samples: int = None,):
     """
     Averages the 2D array radially using bilinear interpolation in polar coordinates.
 
@@ -169,6 +169,8 @@ def average_rings2d(array: np.ndarray, axes: tuple[np.ndarray] = None, num_angle
         axes: Tuple of arrays representing the grid axes (ax1, ax2).
         num_samples: Number of radial samples (r) to take.
         num_angles: Number of angular samples (theta).
+        degree_of_symmetry: Avarage within an angular sector 2pi/degree_of_symmetry instead of a whole ring
+        theta0: initial angle for the averaging
 
     Returns:
         radii: Radial distances at which the interpolation is performed.
@@ -190,10 +192,10 @@ def average_rings2d(array: np.ndarray, axes: tuple[np.ndarray] = None, num_angle
 
     for i, r in enumerate(ax):
         # Parametric equations for points on a circle of radius r
-        theta = np.linspace(0, 2 * np.pi, num_angles, endpoint=False)
+        theta = np.linspace(theta0, theta0 + 2 * np.pi / degree_of_symmetry, num_angles, endpoint=False)
         sample_x = r * np.cos(theta)
         sample_y = r * np.sin(theta)
-        allowed_values = (sample_x >= ax[0]) * (sample_y >= ax[0])
+        allowed_values = (sample_x >= ax[0]) * (sample_y >= ax[0]) 
         sample_x = sample_x[allowed_values]
         sample_y = sample_y[allowed_values]
         # Perform bilinear interpolation at these points
@@ -245,6 +247,7 @@ def expand_ring_averages2d(averaged: np.ndarray[int, ...], axes: tuple[np.ndarra
 
     x, y = np.meshgrid(ax1, ax2)
     r = (x ** 2 + y ** 2) ** 0.5
+    phi = np.angle(x + 1j * y)
     ax1, ax2 = ax1[ax1 >= -1e-10], ax2[ax2 >= -1e-10]
     ax = ax1 if ax1[-1] < ax2[-1] else ax2
     shape = r.shape
@@ -254,7 +257,6 @@ def expand_ring_averages2d(averaged: np.ndarray[int, ...], axes: tuple[np.ndarra
         expanded[(r <= ax[i] + 10 ** -10) * (r > r_old)] = averaged[i]
         r_old = ax[i] + 10 ** -10
     return expanded
-
 
 def expand_ring_averages3d(averaged: np.ndarray[tuple[int, int], ...], axes: tuple[np.ndarray, np.ndarray, np.ndarray] = None) -> np.ndarray[tuple[int, int, int], ...]:
     """
@@ -273,7 +275,6 @@ def expand_ring_averages3d(averaged: np.ndarray[tuple[int, int], ...], axes: tup
     for iz in range(axes[2].size):
         expanded[:, :, iz] = expand_ring_averages2d(averaged[:, iz], axes=(axes[0], axes[1]))
     return expanded
-
 
 def estimate_localized_peaks(array, axes):
     """